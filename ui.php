4bfda18c35d301b91843a156a8e72e5985146a61
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2010-10-11 21:57:09 +0000

    UI functions.
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@9 fc7d47d3-c008-acd5-f51f-d19787b8a02f

---
+++ ui.php
@@ -0,0 +1,82 @@
+<?php
+/*
+Copyright 2010 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+function globalCss() {
+	return <<<OUTPUT
+<style>
+BODY { font-family: Arial; }
+.header { border-bottom: 4px groove #17598F; font-size: 3em; color: #17598F; }
+.preheader { font-size: 0.8em; }
+.notification { color: #C00; }
+.tdnum { text-align: right; }
+</style>
+
+OUTPUT;
+}
+
+
+function uiHeader($title = "HTTP Archive", $bNavlinks = true) {
+	$navlinks = "";
+	if ( $bNavlinks ) {
+		$navlinks = <<<OUTPUT
+<div style="float: right; font-size: 0.8em;">
+<a href="index.php">Home</a>
+&nbsp;|&nbsp;
+<a href="viewarchive.php">Archives</a>
+&nbsp;|&nbsp;
+<a href="about.php">About</a>
+</div>
+OUTPUT;
+	}
+	
+	return <<<OUTPUT
+<table cellpadding=0 cellspacing=0 border=0 style="width: 800px;">
+  <tr>
+    <td valign=top style="padding-right: 10px; text-align:center;">
+      <a href="index.php"><img border=0 src='images/httparchive-49x47.png' width=49 height=47 style="margin-top: 4px;"></a>
+<div style="line-height: 90%; margin-top: 2px; font-size: 11pt;">
+<a href="index.php" style="text-decoration: none; color: #17598F; font-weight: bold;">HTTP
+<br>
+Archive
+</a>
+</div>
+    </td>
+    <td valign=top style="width: 100%">
+	  $navlinks
+      <div class=header><nobr><strong>$title</strong></nobr></div>
+    </td>
+  </tr>
+</table>
+
+OUTPUT;
+}
+
+
+function uiFooter() {
+	return <<<OUTPUT
+    <div style="margin: 30px 60px 30px 60px; border-top: 1px solid #CCC; padding-top: 8px; font-size: 0.8em; text-align: center; width: 680px;">
+      <a href="about.php">about</a> |
+      <a href="http://code.google.com/p/httparchive/issues/list">bugs</a> |
+      <a href="http://code.google.com/p/httparchive/source/checkout">code</a> |
+      <a href="http://groups.google.com/group/httparchive/topics">contact us</a>
+
+    </div>
+
+OUTPUT;
+}
+
+?>
