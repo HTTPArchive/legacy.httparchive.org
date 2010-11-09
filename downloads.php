2b55c2bbebf97c216adb9749e195e7bcc94c488d
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2010-11-09 20:48:41 +0000

    wrote some instructions. did not finish CSV dump.
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@101 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- downloads.php
+++ downloads.php
@@ -19,6 +19,18 @@ require_once("ui.php");
 require_once("utils.php");
 
 $gTitle = "Downloads";
+
+/*
+Here's how httparchive_mysqldump.gz was generated:
+  mysqldump -u miscadmin -p<insert password or get prompted> -h mysql.stevesouders.com stevesouderscom_misc pages<dev> requests<dev> > httparchive_mysqldump
+  gzip httparchive_mysqldump
+
+Here's how I did a CSV of Fortune 100 requestsdev:
+select pagesdev.urlShort, requestsdev.urlShort, method, redirectUrl, firstReq, firstHtml, reqHttpVersion, reqHeadersSize, reqBodySize, reqCookieLen, status, respHttpVersion, respHeadersSize, respBodySize, respSize, respCookieLen, mimeType, req_accept, req_accept_charset, req_accept_encoding, req_accept_language, req_connection, req_host, req_if_modified_since, req_if_none_match, req_referer, req_user_agent, resp_accept_ranges, resp_age, resp_cache_control, resp_connection, resp_content_encoding, resp_content_language, resp_content_length, resp_content_location, resp_content_type, resp_date, resp_etag, resp_expires, resp_keep_alive, resp_last_modified, resp_location, resp_pragma, resp_server, resp_transfer_encoding, resp_vary, resp_via, resp_x_powered_by into outfile 'fortune1000.csv' fields terminated by ',' optionally enclosed by '"' lines terminated by '\n' from pagesdev, requestsdev where pagesdev.pageid=requestsdev.pageid and archive="Fortune 1000" and label="Oct 2010";
+
+mysqldump -u miscadmin -p -h mysql.stevesouders.com --fields-terminated-by ',' --fields-optionally-enclosed-by '"' --lines-terminated-by '\n' select pagesdev.urlShort, requestsdev.urlShort, method, redirectUrl, firstReq, firstHtml, reqHttpVersion, reqHeadersSize, reqBodySize, reqCookieLen, status, respHttpVersion, respHeadersSize, respBodySize, respSize, respCookieLen, mimeType, req_accept, req_accept_charset, req_accept_encoding, req_accept_language, req_connection, req_host, req_if_modified_since, req_if_none_match, req_referer, req_user_agent, resp_accept_ranges, resp_age, resp_cache_control, resp_connection, resp_content_encoding, resp_content_language, resp_content_length, resp_content_location, resp_content_type, resp_date, resp_etag, resp_expires, resp_keep_alive, resp_last_modified, resp_location, resp_pragma, resp_server, resp_transfer_encoding, resp_vary, resp_via, resp_x_powered_by from pagesdev, requestsdev where pagesdev.pageid=requestsdev.pageid and archive="Fortune 1000" and label="Oct 2010" stevesouderscom_misc pagesdev requestsdev;
+
+*/
 ?>
 <!doctype html>
 <html>
@@ -38,7 +50,7 @@ All <a href="viewarchive.php">Archive tables</a> and the tables for each site ha
 </p>
 
 <p>
-In addition, you can download the entire are has a <a href="httparchive_mysqldump.gz">compressed mysqldump</a>
+In addition, you can download the entire are has a <a href="httparchive_mysqldump.gz">compressed mysqldump</a>.
 </p>
 
 <?php echo uiFooter() ?>
