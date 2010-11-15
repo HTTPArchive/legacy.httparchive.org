09700774a85294671b057bf485d7ea527c01096f
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2010-11-15 23:28:01 +0000

    list of urls
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@132 fc7d47d3-c008-acd5-f51f-d19787b8a02f

---
+++ websites.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Copyright 2010 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+require_once("utils.php");
+require_once("ui.php");
+
+$gArchive = "All";
+$gLabel = ( array_key_exists("l", $_GET) ? $_GET["l"] : latestLabel($gArchive) );
+$gTitle = "Web Sites";
+$gMaxUrls = 20000;
+?>
+<!doctype html>
+<html>
+<head>
+	<link type="text/css" rel="stylesheet" href="style.css" />
+	
+	<title>HTTP Archive - <?php echo $gTitle ?></title>
+	<meta charset="UTF-8">
+</head>
+
+<body>
+<?php echo uiHeader($gTitle); ?>
+
+<ul>
+<?php
+$query = "select pageid, url from $gPagesTable where archive = '$gArchive' group by url order by urlShort asc limit $gMaxUrls;";
+$result = doQuery($query);
+while ($row = mysql_fetch_assoc($result)) {
+	echo "<li> <a href='viewsite.php?pageid=" . $row['pageid'] . "&a=$gArchive&l=$gLabel'>" . shortenUrl($row['url']) . "</a>\n";
+}
+mysql_free_result($result);
+?>
+</ul>
+
+<?php echo uiFooter() ?>
+
+</body>
+</html>
