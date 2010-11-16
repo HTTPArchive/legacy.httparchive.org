8243f20fdefbdeaebfe8123e7cf4422d840084a4
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2010-11-16 05:14:36 +0000

    remove the archives link
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@144 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- ui.php
+++ ui.php
@@ -35,7 +35,6 @@ function uiHeader($title = "HTTP Archive", $bNavlinks = true, $extraNav='') {
 		$navlinks = '
 <nav>
 	<ul>
-		<li><a href="viewarchive.php">archives</a></li>
 		<li><a href="about.php">about</a></li>
 		<li><a href="http://groups.google.com/group/httparchive/topics">contact</a></li>
 		<li><a href="http://code.google.com/p/httparchive/">code</a></li>
