d84672d40f75652f62199dc3abf6355b0aa2545f
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2010-12-03 01:09:13 +0000

    first admin function: create MySQL tables
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@198 fc7d47d3-c008-acd5-f51f-d19787b8a02f

---
+++ admin.php
@@ -0,0 +1,47 @@
+<?php 
+/*
+Copyright 2010 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+require_once("utils.php");
+require_once("ui.php");
+
+$gTitle = "Admin";
+?>
+<!doctype html>
+<html>
+<head>
+	<link type="text/css" rel="stylesheet" href="style.css" />
+	
+	<title><?php echo $gTitle ?></title>
+	<meta charset="UTF-8">
+</head>
+
+<body>
+<?php echo uiHeader($gTitle); ?>
+
+<a href="admin.php?a=create">create MySQL tables</a>
+
+<?php
+$gAction = ( array_key_exists("a", $_GET) ? $_GET["a"] : "" );
+if ( "create" === $gAction ) {
+	createTables();
+}
+?>
+
+<?php echo uiFooter() ?>
+
+</body>
+</html>
