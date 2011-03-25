3edc076b7e6e7a763e9c9007b70864d011ae43a1
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2011-03-25 06:59:14 +0000

    pull the HAR file on-demand from WPT
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@301 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- harviewer.js
+++ harviewer.js
@@ -1,8 +1,14 @@
 var HARjson = 
 <?php
-require_once("utils.php");
-
-$target = $_GET['f'];
-echo getHarFileContents($target) . ";\n";
+if ( array_key_exists('u', $_GET) ) {
+	$target = $_GET['u'];
+	echo file_get_contents($target) . ";\n";
+}
+else if ( array_key_exists('f', $_GET) ) {
+	// TODO - remove this - not used since March 24, 2011
+	require_once("utils.php");
+	$target = $_GET['f'];
+	echo getHarFileContents($target) . ";\n";
+}
 ?>
 
