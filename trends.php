32f663eb30d690845cf2fbe0ea2e4b4dd9b35371
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2011-03-28 04:52:07 +0000

    combine request & xfer size line plots
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@315 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- trends.php
+++ trends.php
@@ -49,8 +49,8 @@ $gTitle = "Trends";
 <form>
 	<label>Choose URLs:</label>
 	<select onchange='document.location="?s="+escape(this.options[this.selectedIndex].value)'>
-	    <option value='All'<?php echo ( "constant" != $gSet ? " selected" : "" ) ?>> All
-	    <option value='constant'<?php echo ( "constant" == $gSet ? " selected" : "" ) ?>> intersection
+	    <option value='All'<?php echo ( "intersection" != $gSet ? " selected" : "" ) ?>> All
+	    <option value='intersection'<?php echo ( "intersection" == $gSet ? " selected" : "" ) ?>> intersection
 	</select>
 </form>
 </div>
@@ -62,20 +62,7 @@ $gTitle = "Trends";
 </div>
 	
 <?php
-$setWhere = "";
-if ( "constant" === $gSet ) {
-	// Find the set of URLs that are constant across all labels;
-	$numLabels = doSimpleQuery("select count(distinct(label)) from $gPagesTable;");
-	$query = "select url, count(label) as num from $gPagesTable group by url having num = $numLabels;";
-	$result = doQuery($query);
-	$aUrls = array();
-	while ( $row = mysql_fetch_assoc($result) ) {
-		$aUrls[] = $row['url'];
-	}
-	mysql_free_result($result);
-	$setWhere = " and url in ('" . implode("','", $aUrls) . "')";
-}
-
+// The database fields we want to plot.
 $aFields = array(
 				 //"onLoad",
 				 //"renderStart",
@@ -92,92 +79,140 @@ $aFields = array(
 				 "bytesImg",
 				 "numDomains"
 				 );
-$query = "select label, count(*) as numurls";
-foreach($aFields as $field) {
-	$div = ( false === strpos($field, "bytes") ? 1 : 1024 );
-	$query .= ", ROUND(AVG($field)/$div) as $field";
+
+
+
+// Intersection or All?
+$setWhere = "";
+if ( "intersection" === $gSet ) {
+	// Find the set of URLs that are constant across all labels;
+	$numLabels = doSimpleQuery("select count(distinct(label)) from $gPagesTable;");
+	$query = "select url, count(label) as num from $gPagesTable group by url having num = $numLabels;";
+	$result = doQuery($query);
+	$aUrls = array();
+	while ( $row = mysql_fetch_assoc($result) ) {
+		$aUrls[] = $row['url'];
+	}
+	mysql_free_result($result);
+	$setWhere = " and url in ('" . implode("','", $aUrls) . "')";
 }
 
+// Build the query to pull the stats for ALL fields for ALL labels, for example:
+//   select label, count(*) as numurls, ROUND(AVG(PageSpeed)/1) as PageSpeed, ROUND(AVG(reqTotal)/1) as reqTotal, ROUND(AVG(reqHtml)/1) as reqHtml, 
+//     ROUND(AVG(reqJS)/1) as reqJS, ROUND(AVG(reqCSS)/1) as reqCSS, ROUND(AVG(reqImg)/1) as reqImg, ROUND(AVG(bytesTotal)/1024) as bytesTotal, 
+//     ROUND(AVG(bytesHtml)/1024) as bytesHtml, ROUND(AVG(bytesJS)/1024) as bytesJS, ROUND(AVG(bytesCSS)/1024) as bytesCSS, 
+//     ROUND(AVG(bytesImg)/1024) as bytesImg, ROUND(AVG(numDomains)/1) as numDomains from pagesdev where archive = 'All' group by label;
+$query = "select label, count(*) as numurls";
+foreach($aFields as $field) {
+	$divisor = ( false === strpos($field, "bytes") ? 1 : 1024 );
+	$query .= ", ROUND(AVG($field)/$divisor) as $field";
+}
 $query .= " from $gPagesTable where archive = '$gArchive'$setWhere group by label;";
 $result = doQuery($query);
+
+// Separate the stats by label.
 $hStats = array();
 while ( $row = mysql_fetch_assoc($result) ) {
 	$hStats[$row['label']] = $row;
 }
 mysql_free_result($result);
 
-$aLabels = archiveLabels($gArchive, false);
+// Create a chart for each field.
+$aRunNames = archiveLabels($gArchive, false);
 $aAxisLabels = archiveLabels($gArchive, true, "n/j");
 $labels = urlencode(implode("|", $aAxisLabels));
-array_unshift($aFields, "numurls");
-foreach($aFields as $field) {
+
+echo format2LineChart('reqTotal', 'bytesTotal', $hStats, $aRunNames, $labels);
+echo format2LineChart('reqHtml', 'bytesHtml', $hStats, $aRunNames, $labels);
+echo format2LineChart('reqJS', 'bytesJS', $hStats, $aRunNames, $labels);
+echo format2LineChart('reqCSS', 'bytesCSS', $hStats, $aRunNames, $labels);
+echo format2LineChart('reqImg', 'bytesImg', $hStats, $aRunNames, $labels);
+
+foreach(array('numurls', 'PageSpeed', 'numDomains') as $field) {
+	echo formatChart($field, $hStats, $aRunNames, $labels);
+}
+
+
+function format2LineChart($field, $field2, $hStats, $aRunNames, $labels) {
+	$color = fieldColor($field);
+	$suffix = fieldUnits($field);
+	$color2 = fieldColor($field2);
+	$suffix2 = fieldUnits($field2);
+
+	$fieldVals = fieldValues($field, $hStats, $aRunNames, $min, $max, false);
+	$max = intval(intval($max)*1.5);
+	$step = ( $max > 200 ? 100 : ( $max > 100 ? 20 : 10 ) );
+	$fieldVals2 = fieldValues($field2, $hStats, $aRunNames, $min2, $max2, false);
+	$step2 = ( $max2 > 200 ? 100 : ( $max2 > 100 ? 20 : 10 ) );
+	$url = "http://chart.apis.google.com/chart" .
+		"?chd=t:-1|$fieldVals|-1|$fieldVals2" .
+		"&chxl=0:|$labels" .
+		"&chxt=x,y,r" .
+		"&chs=600x300" .
+		"&cht=lxy" .
+		"&chco=$color,$color2" .
+		"&chm=N" . ( $suffix ? "**+$suffix" : "" ) . ",$color,0,,12,,:-8:8" . "|N" . ( $suffix2 ? "**+$suffix2" : "" ) . ",$color2,1,,12,,:-16:8" .
+		"&chds=9,99,$min,$max,9,99,$min2,$max2" .
+		"&chts=$color2,24" .
+		"&chtt=" . urlencode(fieldTitle($field2)) . "+%26+" . urlencode(fieldTitle($field)) . 
+		"&chma=5,5,5,25" .
+		"&chls=1,6,3|1" .
+		"&chxr=1,$min2,$max2,$step2|2,$min,$max,$step" .
+		"&chxs=1,$color2,11.5,-0.5,lt,$color2,$color2|2,$color,11.5,-0.5,lt,$color,$color" .
+		"&chxtc=0,4|1,4" .
+		"&chxp=0&chdl=" . urlencode(fieldTitle($field)) . "|" . urlencode(fieldTitle($field2)) . "+(kB)&chdlp=bv|r" .
+		"";
+
+	return "<div style='margin: 40px 0 60px 0;'><img src=$url></div>\n";
+}
+
+
+function formatChart($field, $hStats, $aRunNames, $labels) {
 	$color = fieldColor($field);
 	$suffix = fieldUnits($field);
 	$url = "http://chart.apis.google.com/chart" .
-		"?chd=t:-1|" . 
-		fieldValues($field, $hStats, $aLabels, $min, $max) . 
+		"?chd=t:-1|" . fieldValues($field, $hStats, $aRunNames, $min, $max) . 
 		"&chxl=0:|$labels&chxt=x&chs=600x300&cht=lxy&chco=$color" .
 		"&chxs=0,676767,11.5,0,lt,676767&chxtc=0,8&chm=N" . ( $suffix ? "**+$suffix" : "" ) .
 		",$color,0,,12,,::8&chds=0,100,$min,$max&chts=$color,32&chtt=" . urlencode(fieldTitle($field)) . "&chls=2&chma=5,5,5,25";
-	echo "<div style='margin: 40px 0 60px 0;'><img src=$url></div>\n";
+	return "<div style='margin: 40px 0 60px 0;'><img src=$url></div>\n";
 }
 
-$stats = $hStats[$gLabel];
 
-
-function fieldValues($field, $hStats, $aLabels, &$min, &$max) {
+function fieldValues($field, $hStats, $aRunNames, &$min, &$max, $bZero = true) {
 	$aValues = array();
-	foreach($aLabels as $label) {
-		$aValues[] = $hStats[$label][$field];
+	foreach($aRunNames as $run) {
+		$aValues[] = $hStats[$run][$field];
 	}
-	findScale($aValues, $min, $max);
+	findScale($aValues, $min, $max, $bZero);
 
 	return implode(",", $aValues);
 }
 
 
-function findScale($aValues, &$min, &$max) {
-	// Power of 10 less than the min - eg 4719 ==> 1000
-	$min = 0;
-	/* Do we ever want to change the min from 0?
+function findScale($aValues, &$min, &$max, $bZero = true) {
 	$minValue = min($aValues);
-	if ( $minValue > 100 ) {
-		$min = pow(10, strlen("$minValue")-1);
+	$maxValue = max($aValues);
+
+	// Power of 10 less than the min - eg 4719 ==> 4000
+	$min = 0;
+	if ( ! $bZero && 20 < $minValue ) {
+		$base = pow(10, strlen("$minValue")-1);
+		$min = $base * intval(floor($minValue/$base)-1);
 	}
-	*/
 
 	// Multiple of power of 10 less than max value - eg 4719 ==> 5000
 	$max = 10;
-	$maxValue = max($aValues);
 	if ( $maxValue > 10 ) {
 		$base = pow(10, strlen("$maxValue")-1);
-		$max = $base * ceil($maxValue/$base);
+		$max = $base * intval(ceil($maxValue/$base));
 	}
 }
 ?>
 
+
+
 <?php echo uiFooter() ?>
-
-<script>
-/*
-function showSnippets() {
-	var parent = document.getElementById('trends');
-	for ( var iSnippet = 0; iSnippet < gaSnippets.length; iSnippet++ ) {
-		newSnippet = document.createElement('div');
-		newSnippet.id = iSnippet;
-		newSnippet.className = "ianswer";
-		newSnippet.innerHTML = gaSnippets[iSnippet];
-		parent.appendChild(newSnippet);
-	}
-}
-
-var trendsjs = document.createElement('script');
-trendsjs.src = "trends.js?l=<?php echo $gLabel ?>";
-trendsjs.onload = showSnippets;
-trendsjs.onreadystatechange = function() { if ( trendsjs.readyState == 'complete' || trendsjs.readyState == 'loaded' ) { showSnippets(); } };
-document.getElementsByTagName('head')[0].appendChild(trendsjs);
-*/
-</script>
 
 </body>
 
