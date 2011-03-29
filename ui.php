4e58a8e68e717298819c6fb7d04c426dbe415fb6
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2011-03-29 21:00:59 +0000

    add headfirst function for Google Analytics
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@318 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- ui.php
+++ ui.php
@@ -29,6 +29,27 @@ OUTPUT;
 }
 
 
+// HTML to insert first inside the HEAD element.
+function headfirst() {
+	// Google Analytics
+	return <<<OUTPUT
+<script type="text/javascript">
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-22381566-1']);
+_gaq.push(['_setDomainName', '.httparchive.org']);
+_gaq.push(['_trackPageview']);
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+
+OUTPUT;
+}
+
+
 function uiHeader($title = "HTTP Archive", $bNavlinks = true, $extraNav='') {
 	$navlinks = "";
 	if ( $bNavlinks ) {
