3ea0d0f72057d4bcdceb574bd9448278264e5a7e
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2011-05-04 22:35:24 +0000

    switch from beta to normal ga.js
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@428 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- ui.php
+++ ui.php
@@ -42,7 +42,7 @@ _gaq.push(['_trackPageview']);
 _gaq.push(['_trackPageLoadTime']);
 (function() {
 	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/u/ga_beta.js';
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 </script>
