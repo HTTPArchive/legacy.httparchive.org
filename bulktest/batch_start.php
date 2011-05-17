4efa62ececdc106b4356a954473515bb178ce590
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2011-05-17 22:51:37 +0000

    Be clear about requiring utils.php. Move functions that are only used in batch_start to this file from batch_lib. Fix indenting and brackets. Remove most of HAR file stuff.
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@473 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- bulktest/batch_start.php
+++ bulktest/batch_start.php
@@ -15,31 +15,76 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+require_once("../utils.php");
 require_once("batch_lib.inc");
 require_once("bootstrap.inc");
 
+
+// Load the URLs in urls.txt file into status table.
+function loadUrlsFromFile($label) {
+	global $locations, $gArchive;
+
+	$urls = file('./urls.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+	foreach( $urls as $url ) {
+		$url = trim($url);
+		if( strlen($url) ) {
+			$url = refineUrl($url);
+			foreach ( $locations as $location ) {
+				insertToStatusTable($url, $location, $label);
+			}
+		}
+	}
+}
+
+
+// Insert a row into the status table.
+function insertToStatusTable($url, $loc, $label) {
+	global $gStatusTable, $gArchive;
+	$now = time();
+	$cmd = "REPLACE INTO $gStatusTable SET url = '" . mysql_real_escape_string($url) . 
+		"' , location = '" . mysql_real_escape_string($loc) . 
+		"', archive = '$gArchive', label = '$label', status = '" . NOT_STARTED . "', timeofLastChange = '$now';";
+	doSimpleCommand($cmd);
+}
+
+
+function emptyStatusTable() {
+	global $gStatusTable;
+	$cmd = "TRUNCATE TABLE $gStatusTable;";
+	doSimpleCommand($cmd);
+}
+
+
 function killProcessAndChildren($pid, $signal=9) { 
 	exec("ps -ef | awk '\$3 == '$pid' { print  \$2 }'", $output, $ret); 
-	if ( $ret ) 
+	if ( $ret ) {
 		return 'you need ps, grep, and awk'; 
+	}
+
 	while ( list(, $t) = each($output) ) { 
 		if ( $t != $pid ) { 
 			killProcessAndChildren($t,$signal); 
 		} 
 	} 
-        posix_kill($pid, $signal); 
+
+	posix_kill($pid, $signal); 
 }
 
 
 function getPid($text) {
 	exec("ps -ef | grep '$text' | grep -v 'grep $text' | awk '{ print \$2 }'", $output, $ret);
-	if ( $ret )
+	if ( $ret ) {
 		return -1;
-	else
-		if ( count($output) )
+	}
+	else {
+		if ( count($output) ) {
 			return $output[0];
-		else
+		}
+		else {
 			return 0;
+		}
+	}
 }
 
 
@@ -52,14 +97,22 @@ if ( !flock($fp, LOCK_EX | LOCK_NB) ) {
 		killProcessAndChildren($pid);
 	}
 }
+
 // Create all the tables if they are not there.
 createTables();
+
 // Report the summary of the tests
 echo "PREVIOUS TEST:\n";
 reportSummary();
+
 // Empty the status table
 emptyStatusTable();
+
 // Load the next batch
-loadUrlsFromFile();
+// WARNING: Two runs submitted on the same day will have the same label.
+$date = getdate();
+$label = substr($date['month'], 0, 3) . " " . $date['mday'] . " " . $date['year'];
+loadUrlsFromFile($label);
+
 echo "DONE submitting batch run\n";
 ?>
