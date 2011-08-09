9101f0e41973de74bb32874cb7bdcc16319069f9
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2011-08-09 19:12:30 +0000

    update list of dump files
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@518 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- downloads.php
+++ downloads.php
@@ -21,6 +21,7 @@ require_once("utils.php");
 $gTitle = "Downloads";
 
 $gaNormalFiles = array(
+                       array( "httparchive_Aug_1_2011.gz", "August 1 2011" ),
                        array( "httparchive_Jul_15_2011.gz", "July 15 2011" ),
                        array( "httparchive_Jul_1_2011.gz", "July 1 2011" ),
                        array( "httparchive_Jun_15_2011.gz", "June 15 2011" ),
@@ -44,6 +45,7 @@ $gaNormalFiles = array(
                       );
 
 $gaMobileFiles = array(
+                       array( "httparchive_mobile_Aug_1_2011.gz", "Aug 1 2011" ),
                        array( "httparchive_mobile_Jul_15_2011.gz", "July 15 2011" ),
                        array( "httparchive_mobile_Jul_1_2011.gz", "July 1 2011" ),
                        array( "httparchive_mobile_Jun_15_2011.gz", "June 15 2011" ), 
