b14d973b1390c02683433e94afa8ccb5da13dc95
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2011-10-07 17:14:52 +0000

    remove some comments - see if $gRev gets updated
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@606 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- gendata.php
+++ gendata.php
@@ -1,62 +1,10 @@
 <?php
 /*
-label - Oct 1 2011
-device - iphone43, isim, ie8
-set - Top 100, intersection, All
 ?location - Dulles, UK, 
-version - version of the stats code so we know what to flush
-
-trends (average)
-  numurls
-  PageSpeed
-  numDomains
-  bytesTotal
-  bytesHtml
-  bytesJS
-  bytesCSS
-  bytesImg
-  bytesFlash
-  bytesOther
-  reqTotal
-  reqHtml
-  reqJS
-  reqCSS
-  reqImg
-  reqFlash
-  reqOther
-% of image responses of a specific image format
-  pieGif
-  pieJpg
-  piePng
-
-simple yes/no percents:
-  redirects - 64 [% of websites that have at least one redirect]
-  errors - 27 [% of websites that have at least one error]
-  flash - [% of websites that have at least one Flash resource]
-  google libraries API
-
-https - 27 [% of requests that are https]
-
-maxage:
-  binNull
-  binLEZero
-  bin0to1
-  bin1to30
-  bin30to365
-  binGt365
-
-topjslibs - lib1,per1|lib2,per2 - eg, +1 button
-
-onloadCorrelation - field1|cc1,field2|cc2 in desc order by cc
-renderCorrelation - field1|cc1,field2|cc2 in desc order by cc
-
-====================
-
 ?most404s
 ?servers - server1|per1,server2|per2,
 ?topscripts - script1,per1|script2,per2 - a full URL and percent of sites that use it
 */
-
 
 $gRev = '$Rev: 536 $';
 
