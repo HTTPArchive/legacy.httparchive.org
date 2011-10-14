e0d0daf33accd7f966f193d47d336e961006d978
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2011-10-14 21:13:07 +0000

    replace interesting.js with charts.inc - yay!
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@646 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ The <a href="http://code.google.com/p/httparchive/source/checkout">HTTP Archive 
 <td id=leftarrow class=arrow></td>
 <td>
 <div id=interesting>
-<!-- interesting.js will insert interesting stats here -->
+<!-- chart will be inserted here -->
 </div>
 </td>
 <td id=rightarrow class=arrow></td>
@@ -74,9 +74,129 @@ The <a href="http://code.google.com/p/httparchive/source/checkout">HTTP Archive 
 </center>
 
 <script type="text/javascript">
+// HTML strings for each image
+var gaSnippets = new Array();
+
 <?php
-require_once("interesting.js");
+require_once("stats.inc");
+require_once("charts.inc");
+$hStats = getStats(latestLabel("All"), "All", ($gbMobile ? "iphone" : "IE8"));
 ?>
+gaSnippets.push("<?php echo bytesContentTypeChart($hStats) ?>");
+gaSnippets.push("<?php echo responseSizes($hStats) ?>");
+gaSnippets.push("<?php echo percentGoogleLibrariesAPI($hStats) ?>");
+gaSnippets.push("<?php echo percentFlash($hStats) ?>");
+gaSnippets.push("<?php echo popularImageFormats($hStats) ?>");
+gaSnippets.push("<?php echo maxage($hStats) ?>");
+gaSnippets.push("<?php echo percentByProtocol($hStats) ?>");
+gaSnippets.push("<?php echo requestErrors($hStats) ?>");
+gaSnippets.push("<?php echo redirects($hStats) ?>");
+gaSnippets.push("<?php echo correlationChart($hStats, "onLoad") ?>");
+gaSnippets.push("<?php echo correlationChart($hStats, "renderStart") ?>");
+
+
+// The DOM element that is created from each snippet.
+var gaSnippetElems = new Array();
+var curSnippet;
+
+function showSnippet(parentId, bPrev) {
+	var parent = document.getElementById(parentId);
+	if ( ! parent ) {
+		return;
+	}
+
+	var iSnippet = Math.floor(gaSnippets.length * Math.random());
+	if ( curSnippet ) {
+		iSnippet = parseInt(curSnippet.id)
+		//fade(curSnippet, true);
+		curSnippet.style.display = 'none';
+	}
+
+	iSnippet = ( bPrev ? iSnippet-1 : iSnippet+1 );
+	if ( iSnippet >= gaSnippets.length ) {
+		iSnippet = 0;
+	}
+	else if ( iSnippet < 0 ) {
+		iSnippet = gaSnippets.length - 1;
+	}
+
+	var newSnippet = gaSnippetElems[iSnippet];
+	if ( "undefined" === typeof(newSnippet) ) {
+		newSnippet = document.createElement('div');
+		newSnippet.id = iSnippet;
+		gaSnippetElems[iSnippet] = newSnippet;
+		newSnippet.innerHTML = "<a class=image-link href='interesting.php'>" + gaSnippets[iSnippet] + "</a>";
+		var aPosition = findPos(parent);
+		newSnippet.style.left = aPosition[0] + "px";
+		newSnippet.style.top = aPosition[1] + "px";
+		parent.appendChild(newSnippet);
+	}
+	else {
+		newSnippet.style.display = "block";
+	}
+
+	curSnippet = newSnippet;
+	fade(newSnippet);
+}
+
+
+function insertNav(parentId) {
+	var arrow = document.getElementById('leftarrow');
+	if ( arrow ) {
+		arrow.innerHTML = "<a class='image-link' href='javascript:showSnippet(\"" + parentId + "\", 1)'><img src='images/tri-lft-t-14x28.gif' width=14 height=28 border=0></a>";
+	}
+
+	arrow = document.getElementById('rightarrow');
+	if ( arrow ) {
+		arrow.innerHTML = "<a class='image-link' href='javascript:showSnippet(\"" + parentId + "\")'><img src='images/tri-rt-t-14x28.gif' width=14 height=28 border=0></a>";
+	}
+}
+
+
+// opacity is a number 0-100 inclusive
+function fade(idOrElem, bOut) {
+	var elem = idOrElem;
+	if ( "string" === typeof(idOrElem) || "number" === typeof(idOrElem) ) {
+		elem = document.getElementById(idOrElem);
+	}
+	if ( ! elem ) {
+		return;
+	}
+
+	opacity = ( elem.style.opacity ? parseInt(elem.style.opacity * 100) : ( bOut ? 100 : 0 ) );
+	opacity = ( bOut ? opacity - 10 : opacity + 10 );
+	opacity = ( 100 < opacity ? 100 : ( 0 > opacity ? 0 : opacity ) );
+
+	elem.style.opacity = opacity/100;
+	elem.style.filter = "alpha(opacity = " + opacity + ")";
+
+	if ( (bOut && 0 < opacity ) || ( !bOut && 100 > opacity) ) {
+		setTimeout(function() { fade(elem, bOut); }, 50);
+	}
+	else if ( bOut ) {
+		elem.style.display = "none";
+	}
+}
+
+
+// from http://www.quirksmode.org/js/findpos.html
+function findPos(obj) {
+	var curleft = curtop = 0;
+	if (obj.offsetParent) {
+		do {
+			curleft += obj.offsetLeft;
+			curtop += obj.offsetTop;
+		} while (obj = obj.offsetParent);
+	}
+	return [curleft,curtop];
+}
+
+
+function dprint(msg) {
+	if ( "undefined" != typeof(console) ) {
+		console.log(msg);
+	}
+}
 
 showSnippet('interesting'); 
 insertNav('interesting');
