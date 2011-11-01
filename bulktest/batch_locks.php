5be06283df9a7820a15ff711fe3744ab51f436a8
stevesoudersorg <stevesoudersorg@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2011-11-01 18:29:35 +0000

    check which files are locked
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@733 fc7d47d3-c008-acd5-f51f-d19787b8a02f

---
+++ bulktest/batch_locks.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Copyright 2010 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+require_once("bootstrap.inc");
+require_once("../utils.inc");
+require_once("batch_lib.inc");
+
+$aTasks = array("submit", "status", "obtain", "parse");
+$aDevices = array("IE8", "iphone");
+
+foreach ( $aDevices as $device ) {
+	foreach ( $aTasks as $task ) {
+		// lock file for this specific task.
+		$lockfile = lockFilename($device, $task);
+		$fp = fopen($lockfile, "w+");
+		if ( ! flock($fp, LOCK_EX | LOCK_NB) ) {
+			// this task is still running
+			echo "Task \"$task\" locked: $lockfile\n";
+		}
+	}
+}
+
+?>
