a6420c748d139c00bd9ad502a0f2bedfe29a01e1
stevesoudersorg@gmail.com <stevesoudersorg@gmail.com@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2012-01-31 01:51:07 +0000

    add Torbit as a sponsor
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@829 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- about.php
+++ about.php
@@ -297,16 +297,29 @@ The HTTP Archive is possible through the support of these sponsors:
 <a title="O'Reilly Media" href="http://oreilly.com/">O&#8217;Reilly Media</a>,
 <a href="http://www.etsy.com/">Etsy</a>,
 <a title="Strangeloop Networks" href="http://www.strangeloopnetworks.com/">Strangeloop</a>,
-and <a title="dynaTrace Software" href="http://www.dynatrace.com/">dynaTrace Software</a>.
+<a title="dynaTrace Software" href="http://www.dynatrace.com/">dynaTrace Software</a>,
+and
+<a title="Torbit" href="http://torbit.com/">Torbit</a>.
 </p>
-<div><a title="Google" href="http://www.google.com/"><img class="alignnone" title="Google" src="http://httparchive.org/images/google.gif" alt="" width="120" height="40" /></a> <a style="margin-left: 10px;" title="Mozilla" href="http://www.mozilla.org/firefox"><img class="alignnone" title="Mozilla" src="http://httparchive.org/images/mozilla.png" alt="" width="145" height="100" /></a> <a style="margin-left: 10px;" title="New Relic" href="http://www.newrelic.com/"><img class="alignnone" title="New Relic" src="http://httparchive.org/images/newrelic.gif" alt="" width="120" height="22" /></a> <a style="margin-left: 10px;" title="O'Reilly Media" href="http://oreilly.com/"><img class="alignnone" title="O'Reilly Media" src="http://httparchive.org/images/oreilly.gif" alt="" width="155" height="35" /></a></div>
-<div style="margin-top: 20px;"><a href="http://www.etsy.com/"><img class="alignnone" title="Etsy" src="http://httparchive.org/images/etsy.png" alt="" width="106" height="61" /></a> <a style="margin-left: 10px; vertical-align: top;" title="Strangeloop Networks" href="http://www.strangeloopnetworks.com/"><img class="alignnone" title="Strangeloop" src="http://httparchive.org/images/strangeloop.gif" alt="" width="120" height="25" /></a> <a style="margin-left: 10px;" title="dynaTrace Software" href="http://www.dynatrace.com/"><img class="alignnone" title="dynaTrace Software" src="http://httparchive.org/images/dynatrace.gif" alt="" width="120" height="31" /></a></div>
+
+<div>
+<a class=image-link title="Google" href="http://www.google.com/"><img class="alignnone" title="Google" src="http://httparchive.org/images/google.gif" alt="" width="120" height="40" /></a> 
+<a class=image-link style="margin-left: 10px;" title="Mozilla" href="http://www.mozilla.org/firefox"><img class="alignnone" title="Mozilla" src="http://httparchive.org/images/mozilla.png" alt="" width="145" height="100" /></a> 
+<a class=image-link style="margin-left: 10px;" title="New Relic" href="http://www.newrelic.com/"><img class="alignnone" title="New Relic" src="http://httparchive.org/images/newrelic.gif" alt="" width="120" height="22" /></a> 
+<a class=image-link style="margin-left: 10px;" title="O'Reilly Media" href="http://oreilly.com/"><img class="alignnone" title="O'Reilly Media" src="http://httparchive.org/images/oreilly.gif" alt="" width="155" height="35" /></a></div>
+<div style="margin-top: 20px;">
+<a class=image-link href="http://www.etsy.com/"><img class="alignnone" title="Etsy" src="http://httparchive.org/images/etsy.png" alt="" width="106" height="61" /></a> 
+<a class=image-link style="margin-left: 10px; vertical-align: top;" title="Strangeloop Networks" href="http://www.strangeloopnetworks.com/"><img class="alignnone" title="Strangeloop" src="http://httparchive.org/images/strangeloop.gif" alt="" width="120" height="25" /></a> 
+<a class=image-link style="margin-left: 10px;" title="dynaTrace Software" href="http://www.dynatrace.com/"><img class="alignnone" title="dynaTrace Software" src="http://httparchive.org/images/dynatrace.gif" alt="" width="120" height="31" /></a>
+<a class=image-link style="margin-left: 20px;" title="Torbit" href="http://torbit.com/"><img style="padding-bottom: 20px;" class="alignnone" title="Torbit" src="http://httparchive.org/images/torbit_120.png" alt="" width="120" height="25" /></a>
+</div>
 
 
 			   <h2 id=donate>How do I make a donation to support the HTTP Archive?</h2>
 <p>
 The HTTP Archive is part of the Internet Archive, a 501(c)(3) non-profit.
-Donations in support of the HTTP Archive can be made through the Internet Archive's <a href="http://www.archive.org/donate/index.php">donation page</a>.
+Donations in support of the HTTP Archive can be made through the Internet Archive's 
+<a href="http://www.archive.org/donate/index.php">donation page</a>.
 Make sure to designate your donation is for the "HTTP Archive".
 </p>
 
