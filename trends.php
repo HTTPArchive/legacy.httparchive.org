b2e75132a5adf277ed1b74e14e3455859edbc716
Jared Hirsch <ohai@6a68.net>
2012-03-02 00:01:38 -0800

    sloppy but whatevs, trends.php moved barely into bootstrap wrapper

--- trends.php
+++ trends.php
@@ -15,6 +15,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+function cssPath($file) { return "bootstrap/bootstrap/css/" . $file; }
+function jsPath($file) { return "bootstrap/bootstrap/js/" . $file; }
 require_once("ui.inc");
 require_once("utils.inc");
 
@@ -24,49 +26,149 @@ $gTitle = "Trends";
 $gMinLabel = ( array_key_exists("minlabel", $_GET) ? $_GET['minlabel'] : "" );
 $gMaxLabel = ( array_key_exists("maxlabel", $_GET) ? $_GET['maxlabel'] : latestLabel() );
 ?>
-<!doctype html>
-<html>
-<head>
-<title><?php echo genTitle($gTitle) ?></title>
-<meta charset="UTF-8">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>HTTPArchive</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-<?php echo headfirst() ?>
-<link type="text/css" rel="stylesheet" href="style.css" />
-</head>
+    <!-- Le styles -->
+    <link href="<?php echo cssPath('bootstrap.css'); ?>" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
 
-<body>
-
-<?php echo uiHeader($gTitle); ?>
-
-<h1>Trends</h1>
-
-
-<form>
-<div>
-	<label>Choose URLs:</label>
-<?php
-echo selectSlice($gSlice, "", "s");
-?>
-
-<label style="margin-left: 1em;">Start:</label>
-	<?php echo selectArchiveLabel($gArchive, $gMinLabel, false, false, "minlabel"); ?>
-
-<label style="margin-left: 1em;">End:</label>
-	<?php echo selectArchiveLabel($gArchive, $gMaxLabel, false, false, "maxlabel"); ?>
-<input style="margin-left: 1em;" class=button type=submit value="Submit">
-</div>
-</form>
-
-<?php
-$gUrl = NULL; // TODO - get rid of this
-require_once('trends.inc');
-?>
+    /* just doing this to push the thing under the subnav when you click the in-page anchor */
+    .image-link img { padding-top: 60px; }
 
 
 
-<?php echo uiFooter() ?>
 
-</body>
 
+
+    </style>
+    <link href="<?php echo cssPath('bootstrap-responsive.css'); ?>" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="<?php echo jsPath('html5.js'); ?>"></script>
+    <![endif]-->
+
+    <?php /* TODO - do we have touch icons? */ ?>
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="index.php">HTTP Archive</a>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li><a href="index.php">Home</a></li>
+              <li class="active"><a href="trends.php">Trends</a></li>
+              <li><a href="interesting.php">Stats</a></li>
+              <li><a href="websites.php">Websites</a></li>
+              <li><a href="about.php">About</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    <div class="subnav">
+      <ul class="nav nav-pills">
+        <li><a href="#numurls">URLs Analyzed</a></li>
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Bytes/reqs transferred <b class="caret">X</b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#bytesTotal&reqTotal">Total bytes/reqs</a></li>
+            <li><a href="#bytesHtml&reqHtml">HTML bytes/reqs</a></li>
+            <li><a href="#bytesJS&reqJS">JS bytes/reqs</a></li>
+            <li><a href="#bytesCSS&reqCSS">CSS bytes/reqs</a></li>
+            <li><a href="#bytesImg&reqImg">Image bytes/reqs</a></li>
+          </ul>
+        </li>
+
+        <li><a href="#PageSpeed">Page Speed</a></li>
+        <li><a href="#numDomains"># domains</a></li>
+        <li><a href="#perGlibs">Goog libs</a></li>
+        <li><a href="#perFlash">Flash</a></li>
+        <li><a href="#perFonts">Goog fonts</a></li>
+        <li><a href="#maxageNull">Maxagenull</a></li>
+        <li><a href="#perHttps">Https</a></li>
+        <li><a href="#perErrors">Errors</a></li>
+        <li><a href="#perRedirects">Redirects</a></li>
+      </ul>
+    </div>
+    </div>
+<div class="container">
+
+
+
+        <h1>Trends</h1>
+
+
+        <form>
+        <div>
+            <label>Choose URLs:</label>
+        <?php
+        echo selectSlice($gSlice, "", "s");
+        ?>
+
+        <label style="margin-left: 1em;">Start:</label>
+            <?php echo selectArchiveLabel($gArchive, $gMinLabel, false, false, "minlabel"); ?>
+
+        <label style="margin-left: 1em;">End:</label>
+            <?php echo selectArchiveLabel($gArchive, $gMaxLabel, false, false, "maxlabel"); ?>
+        <input style="margin-left: 1em;" class=button type=submit value="Submit">
+        </div>
+        </form>
+
+        <?php
+        $gUrl = NULL; // TODO - get rid of this
+        require_once('trends.inc');
+        ?>
+
+    </div>
+      <hr>
+
+      <footer>
+        <p>The HTTP Archive is <a href="about.php#sponsors">sponsored by</a> 
+        <a title="Google" href="http://www.google.com/">Google</a>,
+        <a title="Mozilla" href="http://www.mozilla.org/firefox">Mozilla</a>,
+        <a title="New Relic" href="http://www.newrelic.com/">New Relic</a>,
+        <a title="O'Reilly Media" href="http://oreilly.com/">O&#8217;Reilly Media</a>,
+        <a href="http://www.etsy.com/">Etsy</a>,
+        <a title="Strangeloop Networks" href="http://www.strangeloopnetworks.com/">Strangeloop</a>,
+        <a title="dynaTrace Software" href="http://www.dynatrace.com/">dynaTrace Software</a>, and
+        <a title="Torbit" href="http://torbit.com/">Torbit</a>, and powered by <a href="http://www.webpagetest.org">WebPagetest</a>.
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?php echo jsPath('../../docs/assets/js/jquery.js'); ?>"></script>
+    <script src="<?php echo jsPath('bootstrap.js'); ?>"></script>
+<script type="text/javascript">
+
+    $('.dropdown-toggle').dropdown();
+
+</script>
+  </body>
 </html>
-
