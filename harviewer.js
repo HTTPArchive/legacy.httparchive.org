6ee5be56e4ab92ec904ae72849fc9dc33d2cf737
stevesoudersorg@gmail.com <stevesoudersorg@gmail.com@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2012-05-31 21:35:18 +0000

    nit
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@897 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- harviewer.js
+++ harviewer.js
@@ -1,13 +1,13 @@
 var HARjson = 
 <?php
 if ( array_key_exists('u', $_GET) ) {
-	$target = $_GET['u'];
+	$target = $_GET["u"];
 	echo file_get_contents($target) . ";\n";
 }
 else if ( array_key_exists('f', $_GET) ) {
 	// TODO - remove this - not used since March 24, 2011
 	require_once("utils.inc");
-	$target = $_GET['f'];
+	$target = $_GET["f"];
 	echo getHarFileContents($target) . ";\n";
 }
 ?>
