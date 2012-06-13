6e017fbe3ae5be2c6b37b1f4eb920f3eeab7c136
stevesoudersorg@gmail.com <stevesoudersorg@gmail.com@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2012-06-13 18:46:58 +0000

    add Charlie Clark
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@917 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- about.php
+++ about.php
@@ -278,6 +278,7 @@ I've received patches from several individuals including
 <a href="http://www.ioncannon.net/">Carson McDonald</a>, 
 <a href="http://jbyers.com/">James Byers</a>,
 <a href="http://greenido.wordpress.com/">Ido Green</a>,
+<a href="http://www.clark-consulting.eu/">Charlie Clark</a>,
  and Mike Pfirrmann.
 Guy Leech helped early on with the design.
 More recently, <a href="https://twitter.com/#!/stephenhay">Stephen Hay</a> created the new logo.
