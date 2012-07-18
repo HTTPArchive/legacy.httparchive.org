6c6782ee52067a91096156ee93959c125dad4c28
stevesoudersorg@gmail.com <stevesoudersorg@gmail.com@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2012-07-18 22:45:03 +0000

    Major Change: instead of autocomplete select list, make the websites.php page a form that gets submitted. punt jQuery. remove unnecessary styles.
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@934 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- websites.php
+++ websites.php
@@ -37,166 +37,52 @@ $gMaxUrls = 20000;
 <body>
 <?php echo uiHeader($gTitle); ?>
 
-<style>
-.websites { 
-	list-style-type: none; }
-#alphaindex { 
-	font-size: 0.9em;
-	text-align: center;
-	position: fixed; 
-	left: 0; }
-@media all and (max-height: 805px) {
-	#alphaindex {
-		overflow-y: scroll;
-	}
-}
-@media all and (min-height: 300px) and (max-height: 400px) {
-	#alphaindex {
-		height: 106px;
-	}
-}
-@media all and (min-height: 400px) and (max-height: 500px) {
-	#alphaindex {
-		height: 206px;
-	}
-}
-@media all and (min-height: 500px) and (max-height: 600px) {
-	#alphaindex {
-		height: 306px;
-	}
-}
-@media all and (min-height: 600px) and (max-height: 700px) {
-	#alphaindex {
-		height: 406px;
-	}
-}
-@media all and (min-height: 700px) and (max-height: 800px) {
-	#alphaindex {
-		height: 506px;
-	}
-}
-@media all and (min-height: 800px) and (max-height: 888px) {
-	#alphaindex {
-		height: 606px;
-	}
-}
-#alphaindex > UL { 
-	list-style-type: none; }
-#alphaindex a {
-	font-weight: bold;
-	color: #004D92;
-	padding: 0 20px 0 10px; }
-#alphaindex a:focus, #alphaindex a:hover {
-	color: #004D92;
-    border-bottom: 0; }
-#alphaindex li {
-	border: 1px solid #FFF;
-	margin-bottom: 0; }
-#alphaindex li:focus, #alphaindex li:hover {
-	border: 1px solid #3C7DC8; }
-#alphaindex .selected {
-	background: #3C7DC8; }
-#alphaindex .selected A {
-	color: #FFF; }
-#alphaindex LI.selected {
-	border: 1px solid #3C7DC8; }
-</style>
+<form onsubmit="doSubmit(); return false;">
+<div style="font-size: 1em;">
+  <label style="vertical-align: baseline;">URL: </label>
+  <input id="url" style="vertical-align: baseline; margin-left: 1em;" size=40/>
+  <input type=submit value="Submit" style="vertical-align: baseline; margin-left: 1em;">
+</div>
+</form>
 
-
-<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/themes/cupertino/jquery-ui.css"/>
-<script>
-function loadScript(url) {
-	var sNew = document.createElement("script");
-	sNew.asyn = true;
-	sNew.src = url;
-	var s0 = document.getElementsByTagName('script')[0];
-	s0.parentNode.insertBefore(sNew, s0);
-}
-
-loadScript("http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js");
-</script>
-<style>
-.ui-autocomplete-loading { background: white url('images/ui-anim_basic_16x16.gif') right center no-repeat; }
-</style>
-<script>
-// Need to avoid race condition with load jQuery async.
-window.onload = function() {
-    $(function() {
-		$( "#site" ).autocomplete({
-				source: "findurl.php",
-                    minLength: 2,
-                    select: function( event, ui ) {
-					document.location = "viewsite.php?pageid=" + ui.item["data-pageid"];
-				}
-			});
-	});
-};
-</script>
-
-<style>
-TABLE { border: 0; width: auto; }
-tr:nth-child(2n+1) td { background: none; }
-TD { padding-top: 0; padding-bottom: 0; }
-.ui-widget, .ui-menu-item{ font-size: 1em; }
-</style>
-
-<table cellpadding=0 cellspacing=0 border=0 style="width: auto;">
-<tr>
-<td style="font-weight: bold;">
-<?php
-$query = "select count(distinct(url)) from $gPagesTable where archive = '$gArchive';";
-$count = doSimpleQuery($query);
-echo commaize($count) . " total URLs";
-?>
-</td>
-<td style="padding-left: 40px;">
-  <div class="ui-widget" style="font-size: 1em;">
-    <label for="birds" style="vertical-align: baseline;">find URL: </label>
-    <input id="site" style="vertical-align: baseline;"/>
-  </div>
-</td>
-</tr>
-</table>
-
-<div id=alphaindex>
-<ul>
-  <li<?php echo ( $gChar === "0" ? " class=selected" : "" ) ?>> <a href='?c=0'>0-9</a>
-<?php
-for ( $i = 65; $i <= 90; $i++ ) {
-	$char = chr($i);
-	echo "<li" . ( $gChar === $char ? " class=selected" : "" ) . "> <a href='?c=$char'>$char</a>\n";
-}
-?>
-</ul>
+<div id=urls>
 </div>
 
-<a name='top'></a>
-<ul class=websites>
-<?php
-$query = "select max(pageid) as pageid, url from $gPagesTable where archive = '$gArchive' and (" .
-	( "w" === $gLcChar ? "(url like 'http://w%' and url not like 'http://www.%')" : "url like 'http://$gLcChar%'" ) .
-	" or url like 'http://www.$gLcChar%') group by url order by url asc limit $gMaxUrls;";
-if ( "0" === $gChar ) {
-	$query = "select max(pageid) as pageid, url from $gPagesTable where archive = '$gArchive' and url like 'http://www.%' and url not regexp 'http://www.[a-z]' and url not regexp 'http://www.[A-Z]' group by url order by url asc limit $gMaxUrls;";
-}
-$result = doQuery($query);
-$lastMark = "";
-while ($row = mysql_fetch_assoc($result)) {
-	$url = $row['url'];
-	if ( ereg('http://www\.([a-z])', $url, $regs) ) {
-		$curMark = $regs[1];
-		if ( $curMark != $lastMark ) {
-			echo "<a name='" . strtoupper($curMark) . "'></a>\n";
-			$lastMark = $curMark;
-		}
-	}
-	echo "<li> <a href='viewsite.php?pageid=" . $row['pageid'] . "'>" . shortenUrl($url) . "</a>\n";
-}
-mysql_free_result($result);
-?>
-</ul>
-
 <?php echo uiFooter() ?>
+
+<script>
+var inputUrl = document.getElementById("url");
+
+function doSubmit() {
+	loadScript("findurl.php?term=" + encodeURI(inputUrl.value) + "&jsonp=showResults");
+	spinnerOn();
+}
+
+
+function showResults(aResults) {
+	var sHtml = "";
+	for ( var i=0, len=aResults.length; i < len; i++ ) {
+		var hResult = aResults[i];
+		var url = hResult["label"];
+		var value = hResult["value"];
+		if ( 0 == value ) {
+			sHtml = "<li style='margin-bottom: 1em;'> " + url + "\n" + sHtml;
+			break;
+		}
+		var pageid = hResult["data-pageid"];
+		sHtml += "<li> <a href='viewsite.php?pageid=" + pageid + "'>" + url + "\n";
+	}
+	if ( sHtml ) {
+		document.getElementById("urls").innerHTML = "<ul style='list-style-type: none;'>\n" + sHtml + "</ul>\n";
+	}
+}
+
+
+function spinnerOn() {
+	document.getElementById("urls").innerHTML = "<img src='images/busy.gif'>";
+}
+
+</script>
 
 </body>
 </html>
