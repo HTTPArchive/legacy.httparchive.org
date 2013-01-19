ab27a39b33cb2f196dabc83d8f4ef89ea0e38e20
stevesoudersorg@gmail.com <stevesoudersorg@gmail.com@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2013-01-19 01:18:23 +0000

    Restrict the # of images. Add querystring params for label, (set)TimeOut, and width. Add label select list. Add select list for size. Add a fallback query for crawls that do not have rank.
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@1046 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- patchwork.php
+++ patchwork.php
@@ -6,7 +6,15 @@ require_once("dbapi.inc");
 $gTitle = "Render Patchwork";
 $gTime = getParam('t', '0'); // milliseconds
 $gN = getParam('n', 100); // number of top sites to look at
-if ( $gN <= 0 || 1000 < $gN ) {
+$gL = getParam('l');
+$gTO = getParam('to', 500);
+$gW = getParam('w', 200); // normal dimensions are 138x200 or 90x200
+// mobile images are 200x300, IE are 200x138
+$gH = round( ($gbMobile ? 300 : 138)*$gW/200 );
+if ( ! $gL ) {
+	$gL = latestLabel(); // only do this if we have to
+}
+if ( $gN <= 0 || 100 < $gN ) {
 	$gN = 100;
 }
 $wptServer = wptServer();
@@ -21,14 +29,16 @@ $wptServer = wptServer();
 <link type="text/css" rel="stylesheet" href="style.css" />
 <style>
 BODY { padding: 0; margin: 0; width: 100%; background: #000; }
+A, A:hover { border-bottom: 0px solid #FFF; text-decoration: none; }
+SELECT { vertical-align: super; }
 .square { float: left; }
-#time { margin: 0 1em; }
 </style>
 <script>
 var gTime = <?php echo $gTime ?>;
 var gN = <?php echo $gN ?>;
 var gStep = <?php echo ( $gbMobile ? 1000 : 100 ) ?>;
 var gbPlay = false;
+var gbMobile = <?php echo ( $gbMobile ? "true" : "false" ) ?>;
 
 function forward() {
 	if ( checkReady() ) {
@@ -60,11 +70,8 @@ function play() {
 
 function loop() {
 	if ( gbPlay ) {
-		if ( document.getElementById('time').value > 15000 ) {
-			return;
-		}
 		forward();
-		setTimeout(loop, 200); // it takes about 1 second
+		setTimeout(loop, <?php echo $gTO ?>); // Can't play back in realtime (or can we?).
 	}
 }
 
@@ -144,6 +151,34 @@ function doSubmitTime() {
 	document.location = "patchwork.php?t=" + time + "&n=" + gN;
 }
 
+
+function doSize(w) {
+	if ( ! checkReady() ) {
+		return;
+	}
+
+	if ( ! w ) {
+		var sel = document.getElementById('size');
+		w = parseInt(sel.options[sel.selectedIndex].value);
+		if ( ! w ) {  // 0?
+			return;
+		}
+	}
+	var h = parseInt( (gbMobile ? 300 : 138)*w/200 );
+
+	var allthumbs = document.getElementById('allthumbs');
+	var aImages = allthumbs.getElementsByTagName('img');
+	var len = aImages.length;
+	for ( var i = 0; i < len; i++ ) {
+		var image = aImages[i];
+		image.style.width = w + "px";
+		image.style.height = h + "px";
+		var parent = image.parentNode.parentNode;
+		parent.style.width = w + "px";
+		parent.style.height = h + "px";
+	}
+}
+
 var hPages = {}; // this gets added to by patchwork.js
 </script>
 </head>
@@ -155,24 +190,36 @@ var hPages = {}; // this gets added to by patchwork.js
 -->
 
 <div style="font-size: 2em;">
-<a style="margin-left: 0.6em; border-bottom: 0; font-size: 1.5em; color: #FFF;" href="javascript:back()" title="Back">-</a>
-<a style="margin-left: 0.6em; border-bottom: 0; font-size: 1.5em; color: #FFF;" href="javascript:play()" title="Play" id=playbtn><img src="images/playButton.png" style="vertical-align: middle;"></a>
-<a style="margin-left: 0.6em; border-bottom: 0; font-size: 1.5em; color: #FFF; display: none;" href="javascript:pause()" title="Pause" id=pausebtn><img src="images/pauseButton.png" style="vertical-align: middle;" width=41></a>
-<a style="margin-left: 0.6em; border-bottom: 0; font-size: 1.5em; color: #FFF;" href="javascript:forward()" title="Forward">+</a>
+<a style="margin-left: 0.6em; broder-bottom: 0; font-size: 1.5em; color: #FFF;" href="javascript:back()" title="Back">-</a>
+<a style="margin-left: 0.6em; broder-bottom: 0; font-size: 1.5em; color: #FFF;" href="javascript:play()" title="Play" id=playbtn><img src="images/playButton.png" style="vertical-align: middle;"></a>
+<a style="margin-left: 0.6em; broder-bottom: 0; font-size: 1.5em; color: #FFF; display: none;" href="javascript:pause()" title="Pause" id=pausebtn><img src="images/pauseButton.png" style="vertical-align: middle;" width=41></a>
+<a style="margin-left: 0.6em; broder-bottom: 0; font-size: 1.5em; color: #FFF;" href="javascript:forward()" title="Forward">+</a>
 <form style="margin-left: 0.6em; display:inline;" onsubmit="doSubmitTime();return false;">
 <input id=time type=text size=5 value=<?php echo $gTime ?> style="text-align: right; border-color: #333; margin: 0 0 0.2em 0; background: #000;"> ms
 </form>
-<!--
-<span id=msg style="font-size: 0.7em; margin-left: 1em; color: #C30; font-style: italic;">click "+" to see the screens render</span>
--->
+<form style="margin-left: 0.6em; display:inline; font-size: 0.6em;">
+<select id=size onchange="doSize()">
+<option value=50> tiny
+<option value=100> small
+<option value=200 selected> medium
+<option value=400> large
+</select>
+</form>
+<form style="margin-left: 1em; display:inline; font-size: 0.6em;">
+	<?php echo selectArchiveLabel($gArchive, $gL); ?>
+</form>
 </div>
 
 <div id=allthumbs>
 <?php
 // Display a thumbnail of the Top N websites at a certain time in the loading process.
-$crawl = latestLabel();
-$query = "select rank, pageid, url, wptid, wptrun from $gPagesTable where label='$crawl' and rank > 0 and rank <= " . (2*$gN) . " order by rank asc;";
+$query = "select rank, pageid, url, wptid, wptrun from $gPagesTable where label='$gL' and rank > 0 and rank <= " . (2*$gN) . " order by rank asc;";
 $result = doQuery($query);
+if ( 0 == mysql_num_rows($result) ) {
+	mysql_free_result($result);
+	$query = "select u.rank, pageid, url, wptid, wptrun from $gPagesTable, $gUrlsTable as u where label='$gL' and u.rank > 0 and u.rank <= " . (2*$gN) . " and urlOrig=url order by u.rank asc;";
+	$result = doQuery($query);
+}
 $i = 0;
 while ($row = mysql_fetch_assoc($result)) {
 	$url = $row['url'];
@@ -181,9 +228,12 @@ while ($row = mysql_fetch_assoc($result)) {
 		$wptid = $row['wptid'];
 		$wptrun = $row['wptrun'];
 		$rank = $row['rank'];
-		echo "<div class=square><a href='viewsite.php?pageid=$pageid' title='$url' style='border-bottom: 0;'>" .
-			"<img id=$pageid data-wptid='$wptid' data-wptrun=$wptrun border=0 height=140 src='frame.php?t=$gTime&wptid=$wptid&wptrun=$wptrun'>" .
-			"</a></div>\n";
+		echo "<div style='height: {$gH}px; width: {$gW}px; float: left; background: #FFF;'>" . // show white in case of missing images
+			"<a href='viewsite.php?pageid=$pageid' title='$url' style='broder-bottom: 0;'>" .
+			"<img id=$pageid data-wptid='$wptid' data-wptrun=$wptrun style='border-width: 0; height: {$gH}px; width: {$gW}px;' src='frame.php?t=$gTime&wptid=$wptid&wptrun=$wptrun'>" .
+			"</a>" .
+			"</div>" .
+			"";
 		$i++;
 		if ( $i >= $gN ) {
 			break;
@@ -194,7 +244,7 @@ mysql_free_result($result);
 ?>
 </div>
 
-<script src="patchwork.js?n=<?php echo $gN ?>&crawl=<?php echo $crawl ?>" async></script>
+<script src="patchwork.js?n=<?php echo $gN ?>&l=<?php echo $gL ?>" async></script>
 </body>
 </html>
 
