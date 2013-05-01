73c01b25fd75119322a2f90bef13227a8427b0f5
stevesoudersorg@gmail.com <stevesoudersorg@gmail.com@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2013-05-01 22:26:09 +0000

    add patch for older Blaze.io mobile "0000.jpg" screenshot.
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@1197 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- frame.php
+++ frame.php
@@ -10,6 +10,7 @@ require_once("pages.inc");
 $gTime = getParam('t', '2500'); // milliseconds
 $wptid = getParam('wptid');
 $wptrun = getParam('wptrun');
+$gPageid = getParam('pageid');
 
 $wptServer = wptServer();
 $xmlurl = "{$wptServer}xmlResult.php?test=$wptid";
@@ -31,6 +32,10 @@ if ( $frames->frame ) {
 }
 $f = "0000" . ($tbefore/100);
 $f = substr($f, strlen($f)-4);
+if ( $gbMobile && "0000" == $f && $gPageid < 12217 ) {
+	// There's a bug in Blaze's WPT that calls the "0.0s" image "frame_0010.jpg" instead of "frame_0000.jpg" up until Oct 1, 2011.
+	$f = "0010";
+}
 $imgUrl = "{$wptServer}thumbnail.php?test=$wptid&width=200&file=video_$wptrun/frame_$f.jpg";
 header("Location: $imgUrl");
 ?>
