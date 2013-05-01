9a3f141dad436e78ec2c0cf3f7bc2ed5e439e93f
stevesoudersorg@gmail.com <stevesoudersorg@gmail.com@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2013-05-01 21:53:41 +0000

    Resurrecting this old page that never got announced. Change some var names. Avoid multiple copies of intl Google.coms. Change IE from 100 to 1000 ms screenshot granularity. Improve playback logic to always show most recent screenshot. Improve the forms so that nothing requires reloading the page, but added a "link" anchor that generates the right URL with querystring params.
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@1194 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- patchwork.php
+++ patchwork.php
@@ -4,20 +4,21 @@ require_once("utils.inc");
 require_once("dbapi.inc");
 
 $gTitle = "Render Patchwork";
-$gTime = getParam('t', '0'); // milliseconds
-$gN = getParam('n', 100); // number of top sites to look at
-$gL = getParam('l');
-$gTO = getParam('to', 500);
-$gW = getParam('w', 200); // normal dimensions are 138x200 or 90x200
+$gCurTime = getParam('t', '0'); // the current time frame (0.1, 0.2, 0.3, etc.) in milliseconds
+$gNumUrls = getParam('n', 100); // number of top sites to look at
+if ( $gNumUrls <= 0 || 200 < $gNumUrls ) {
+	// constrain the # of images
+	$gNumUrls = 100;
+}
+$gLabel = getParam('l');        // crawl label
+if ( ! $gLabel ) {
+	$gLabel = latestLabel(); // only do this if we have to
+}
+$gMinStep = ( $gbMobile ? 1000 : 100 ); // finest granularity of screenshots for this browser
+$gStep = getParam('step', ( $gbMobile ? 1000 : 1000 ));  // time interval to step during playback
+$gW = getParam('w', 100); // normal dimensions are 138x200 or 90x200
 // mobile images are 200x300, IE are 200x138
 $gH = round( ($gbMobile ? 300 : 138)*$gW/200 );
-if ( ! $gL ) {
-	$gL = latestLabel(); // only do this if we have to
-}
-if ( $gN <= 0 || 100 < $gN ) {
-	// constrain the # of images
-	$gN = 100;
-}
 $wptServer = wptServer();
 ?>
 <!doctype html>
@@ -31,21 +32,24 @@ $wptServer = wptServer();
 <style>
 BODY { padding: 0; margin: 0; width: 100%; background: #000; }
 A, A:hover { border-bottom: 0px solid #FFF; text-decoration: none; }
+A.img, A.img:hover { border-bottom: 0px solid #FFF; text-decoration: none; }
+A.txt { color: #AAF; text-decoration: underline; }
+A.txt:hover { color: #DAF; text-decoration: underline; border-bottom: 0; }
 SELECT { vertical-align: super; }
 .square { float: left; }
 </style>
 <script>
-var gTime = <?php echo $gTime ?>;
-var gN = <?php echo $gN ?>;
-var gStep = <?php echo ( $gbMobile ? 1000 : 100 ) ?>;
+var gCurTime = <?php echo $gCurTime ?>;
+var gNumUrls = <?php echo $gNumUrls ?>;
+var gStep = <?php echo $gStep ?>;
+var gMinStep = <?php echo $gMinStep ?>;
 var gbPlay = false;
 var gbMobile = <?php echo ( $gbMobile ? "true" : "false" ) ?>;
 
 function forward() {
 	if ( checkReady() ) {
 		if ( ! checkDone() ) {
-			adjustTime(gStep);
-			adjustImages();
+			gotoTime(gCurTime + gStep);
 			checkDone();
 		}
 	}
@@ -54,9 +58,16 @@ function forward() {
 
 function back() {
 	if ( checkReady() ) {
-		adjustTime(-gStep);
-		adjustImages();
+		if ( (gCurTime - gStep) > 0 ) {
+			gotoTime(gCurTime - gStep);
+		}
 	}
+}
+
+
+function gotoTime(t) {
+	setTime(t);
+	adjustImages();
 }
 
 
@@ -72,7 +83,7 @@ function play() {
 function loop() {
 	if ( gbPlay ) {
 		forward();
-		setTimeout(loop, <?php echo $gTO ?>); // Can't play back in realtime (or can we?).
+		setTimeout(loop, <?php echo $gStep ?>); // Can't play back in realtime (or can we?).
 	}
 }
 
@@ -100,19 +111,25 @@ function adjustImage(image) {
 	if ( ! hFrames ) {
 		dprint("ERROR: Pageid " + pageid + " wasn't found in hFrames.");
 	}
-	else if ( hFrames[gTime] ) {
-		// this site has a screen update for the current time
-		var f = "0000" + parseInt(gTime/100);
-		f = f.substring(f.length-4); // eg, "0025" is 2500 ms
-		image.src = "<?php echo $wptServer ?>thumbnail.php?test=" + image.getAttribute("data-wptid") + 
-			"&width=200&file=video_" + image.getAttribute("data-wptrun") + "/frame_" + f + ".jpg";
+	else { 
+		// Iterate backwards through the screenshots to find the right one to show.
+		// Slightly more complicated in order to handle when the "step" interval is greater than the filmstrip interval.
+		for ( var t = gCurTime; t >= 0; t -= gMinStep ) {
+			if ( hFrames[t] ) {
+				var f = "0000" + parseInt(t/100);
+				f = f.substring(f.length-4); // eg, "0025" is 2500 ms
+				image.src = "<?php echo $wptServer ?>thumbnail.php?test=" + image.getAttribute("data-wptid") + 
+					"&width=200&file=video_" + image.getAttribute("data-wptrun") + "/frame_" + f + ".jpg";
+				return;
+			}
+		}
 	}
 }
 
 
-function adjustTime(delta) {
-	gTime += delta;
-	document.getElementById('time').value = gTime;
+function setTime(t) {
+	gCurTime = t;
+	document.getElementById('time').value = gCurTime;
 }
 
 
@@ -132,8 +149,9 @@ function checkReady() {
 }
 
 
+// Return true if the current "time" is greater than the latest screenshot.
 function checkDone() {
-	if ( gTime > msMax ) {
+	if ( gCurTime > msMax ) {  // mxMax is defined in patchwork.js
 		gbPlay = false; // turn off play (if it's on)
 		toggle(true);   // restore play button (if it's not there)
 		alert("Done");
@@ -152,7 +170,19 @@ function toggle(bShowPlay) {
 
 function doSubmitTime() {
 	var time = document.getElementById('time').value;
-	document.location = "patchwork.php?t=" + time + "&n=" + gN;
+	gotoTime(time);
+}
+
+
+function getSize() {
+	var sel = document.getElementById('size');
+	return parseInt(sel.options[sel.selectedIndex].value);
+}
+
+
+function getLabel() {
+	var sel = document.getElementById('sellabel');
+	return sel.options[sel.selectedIndex].value;
 }
 
 
@@ -162,8 +192,7 @@ function doSize(w) {
 	}
 
 	if ( ! w ) {
-		var sel = document.getElementById('size');
-		w = parseInt(sel.options[sel.selectedIndex].value);
+		w = getSize();
 		if ( ! w ) {  // 0?
 			return;
 		}
@@ -184,6 +213,11 @@ function doSize(w) {
 }
 
 
+function gotoLink() {
+	document.location = "patchwork.php?t=" + document.getElementById('time').value + "&w=" + getSize() + "&l=" + getLabel() + "&n=" + gNumUrls;
+}
+
+
 function dprint(msg) {
 	if ( console && console.log ) {
 		console.log(msg);
@@ -196,47 +230,40 @@ var hPages = {}; // this gets added to by patchwork.js
 </head>
 
 <body>
-
-<!--
-<?php echo uiHeader($gTitle); ?>
--->
-
 <div style="font-size: 2em;">
 <a style="margin-left: 0.6em; broder-bottom: 0; font-size: 1.5em; color: #FFF;" href="javascript:back()" title="Back">-</a>
 <a style="margin-left: 0.6em; broder-bottom: 0; font-size: 1.5em; color: #FFF;" href="javascript:play()" title="Play" id=playbtn><img src="images/playButton.png" style="vertical-align: middle;"></a>
 <a style="margin-left: 0.6em; broder-bottom: 0; font-size: 1.5em; color: #FFF; display: none;" href="javascript:pause()" title="Pause" id=pausebtn><img src="images/pauseButton.png" style="vertical-align: middle;" width=41></a>
 <a style="margin-left: 0.6em; broder-bottom: 0; font-size: 1.5em; color: #FFF;" href="javascript:forward()" title="Forward">+</a>
 <form style="margin-left: 0.6em; display:inline;" onsubmit="doSubmitTime();return false;">
-<input id=time type=text size=5 value=<?php echo $gTime ?> style="text-align: right; border-color: #333; margin: 0 0 0.2em 0; background: #000;"> ms
+<input id=time type=text size=5 value=<?php echo $gCurTime ?> style="text-align: right; border-color: #333; margin: 0 0 0.2em 0; background: #000;"> ms
 </form>
 <form style="margin-left: 0.6em; display:inline; font-size: 0.6em;">
 <select id=size onchange="doSize()">
-<option value=50> tiny
-<option value=100> small
-<option value=200 selected> medium
-<option value=400> large
+<option value=50 <?php echo ( 50 == $gW ? "selected" : "" ) ?>> tiny
+<option value=100 <?php echo ( 100 == $gW ? "selected" : "" ) ?>> small
+<option value=200 <?php echo ( 200 == $gW ? "selected" : "" ) ?>> medium
+<option value=400 <?php echo ( 400 == $gW ? "selected" : "" ) ?>> large
 </select>
+<span style="margin-left: 0.6em;">
+	<?php echo selectArchiveLabel($gArchive, $gLabel, true, false); ?>
+</span>
 </form>
-<form style="margin-left: 1em; display:inline; font-size: 0.6em;">
-	<?php echo selectArchiveLabel($gArchive, $gL); ?>
-</form>
+<a href="javascript:gotoLink()" style="margin-left: 1em; font-size: 0.8em;" class=txt>link</a>
 </div>
 
 <div id=allthumbs>
 <?php
 // Display a thumbnail of the Top N websites at a certain time in the loading process.
-//CVSNO $query = "select pageid, url, wptid, wptrun from $gPagesTable where label='$gL' and rank > 0 and rank <= " . (2*$gN) . " order by rank asc;";
-require_once("delme.inc");
-$query = "select pageid, url, wptid, wptrun from $gPagesTable as p, $gUrlsTable as u where label='$gL' and url in $sUrls and url=urlOrig order by u.rank asc limit " . (2*$gN) . ";";
+$limitgoogle = "(url = 'http://www.google.com/' OR url not like '%://www.google.%')"; // There are 10+ sites that all look the same from Google intl sites
+$query = "select pageid, url, wptid, wptrun from $gPagesTable where label='$gLabel' and rank > 0 and rank <= " . (2*$gNumUrls) . " and $limitgoogle order by rank asc;";
 $result = doQuery($query);
-/* CVSNO
 if ( 0 == mysql_num_rows($result) ) {
 	mysql_free_result($result);
 	// Older crawls do NOT have values for "rank". Use today's rank.
-	$query = "select pageid, url, wptid, wptrun from $gPagesTable, $gUrlsTable as u where label='$gL' and u.rank > 0 and u.rank <= " . (2*$gN) . " and urlOrig=url order by u.rank asc;";
+	$query = "select pageid, url, wptid, wptrun from $gPagesTable, $gUrlsTable as u where label='$gLabel' and u.rank > 0 and u.rank <= " . (2*$gNumUrls) . " and urlOrig=url and $limitgoogle order by u.rank asc;";
 	$result = doQuery($query);
 }
-CVSNO */
 $i = 0;
 while ($row = mysql_fetch_assoc($result)) {
 	$url = $row['url'];
@@ -245,13 +272,13 @@ while ($row = mysql_fetch_assoc($result)) {
 		$wptid = $row['wptid'];
 		$wptrun = $row['wptrun'];
 		echo "<div style='height: {$gH}px; width: {$gW}px; float: left; background: #FFF;'>" . // show white in case of missing images
-			"<a href='viewsite.php?pageid=$pageid' title='$url' style='broder-bottom: 0;'>" .
-			"<img id=$pageid data-wptid='$wptid' data-wptrun=$wptrun style='border-width: 0; height: {$gH}px; width: {$gW}px;' src='frame.php?t=$gTime&wptid=$wptid&wptrun=$wptrun'>" .
+			"<a href='viewsite.php?pageid=$pageid' title='$url' class=img>" .
+			"<img id=$pageid data-wptid='$wptid' data-wptrun=$wptrun style='border-width: 0; height: {$gH}px; width: {$gW}px;' src='frame.php?t=$gCurTime&wptid=$wptid&wptrun=$wptrun'>" .
 			"</a>" .
 			"</div>" .
 			"";
 		$i++;
-		if ( $i >= $gN ) {
+		if ( $i >= $gNumUrls ) {
 			break;
 		}
 	}
@@ -260,7 +287,17 @@ mysql_free_result($result);
 ?>
 </div>
 
-<script src="patchwork.js?n=<?php echo $gN ?>&l=<?php echo $gL ?>" async></script>
+<script>
+var sellabel = document.getElementById('sellabel');
+if (sellabel.addEventListener) {
+	sellabel.addEventListener('change', gotoLink, false);
+}
+else if (sellabel.attachEvent) {
+	sellabel.attachEvent('onchange', gotoLink);
+}
+</script>
+
+<script src="patchwork.js?n=<?php echo $gNumUrls ?>&l=<?php echo $gLabel ?>" async></script>
 </body>
 </html>
 
