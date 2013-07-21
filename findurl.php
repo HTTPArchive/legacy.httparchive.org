49c2f28fd66163049aecaaf90e09200a2a962177
stevesoudersorg@gmail.com <stevesoudersorg@gmail.com@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2013-07-21 21:14:24 +0000

    better error handling when the requested URL string is not found.
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@1237 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- findurl.php
+++ findurl.php
@@ -19,50 +19,53 @@ while ( $row = mysql_fetch_assoc($result) ) {
 }
 mysql_free_result($result);
 
-// Second, get the newest pageids for each URL matching the search term AND the urlhashes.
-$query = "select urlShort, max(pageid) as pageid, rank from $gPagesTable where archive='$gArchive' and urlhash in (" .
-	implode(",", $aUrlhashes) . ") and urlShort like '%$term%' group by urlShort order by rank asc limit $maxLimit;";
-$result = doQuery($query);
+$response = "";
+if ( count($aUrlhashes) ) {
+	// Second, get the newest pageids for each URL matching the search term AND the urlhashes.
+	$query = "select urlShort, max(pageid) as pageid, rank from $gPagesTable where archive='$gArchive' and urlhash in (" .
+		implode(",", $aUrlhashes) . ") and urlShort like '%$term%' group by urlShort order by rank asc limit $maxLimit;";
+	$result = doQuery($query);
 
 
-// Only return $maxResults results.
-// If there are more results, return a message with the remaining number.
-// We want to return the HIGHEST RANKED results first. Unfortunately, "asc" returns NULL first, 
-// so we have to wade through those in one array, and store the other results in a different array.
-$aRankedSites = array();
-$aUnrankedSites = array();
-$numUrls = mysql_num_rows($result);
-while ( $row = mysql_fetch_assoc($result) ) {
-	$url = $row['urlShort'];
-	$pageid = $row['pageid'];
-	$rank = $row['rank'];
+	// Only return $maxResults results.
+	// If there are more results, return a message with the remaining number.
+	// We want to return the HIGHEST RANKED results first. Unfortunately, "asc" returns NULL first, 
+	// so we have to wade through those in one array, and store the other results in a different array.
+	$aRankedSites = array();
+	$aUnrankedSites = array();
+	$numUrls = mysql_num_rows($result);
+	while ( $row = mysql_fetch_assoc($result) ) {
+		$url = $row['urlShort'];
+		$pageid = $row['pageid'];
+		$rank = $row['rank'];
 
-	if ( null == $rank ) {
-		if ( count($aUnrankedSites) < $maxResults ) {
-			array_push($aUnrankedSites, array("label" => $url, "value" => $url, "data-pageid" => $pageid));
+		if ( null == $rank ) {
+			if ( count($aUnrankedSites) < $maxResults ) {
+				array_push($aUnrankedSites, array("label" => $url, "value" => $url, "data-pageid" => $pageid));
+			}
+		}
+		else if ( count($aRankedSites) < $maxResults ) {
+			array_push($aRankedSites, array("label" => $url, "value" => $url, "data-pageid" => $pageid));
+		}
+		else {
+			break;
 		}
 	}
-	else if ( count($aRankedSites) < $maxResults ) {
-		array_push($aRankedSites, array("label" => $url, "value" => $url, "data-pageid" => $pageid));
+
+	if ( count($aRankedSites) < $maxResults ) {
+		// add some UNRANKED results to make up the difference
+		array_splice($aRankedSites, count($aRankedSites), $maxResults, $aUnrankedSites);
+	}		
+
+	if ( $numUrls > $maxResults ) {
+		$remaining = $numUrls - $maxResults;
+		array_push($aRankedSites, array("label" => ( $remaining > 100 ? "100+ more URLs" : "$remaining more URLs" ) . ", refine further", "value" => "0"));
 	}
-	else {
-		break;
-	}
+	mysql_free_result($result);
+
+	//echo JSON to our jQueryUI auto-complete box
+	$response = json_encode($aRankedSites);
 }
-
-if ( count($aRankedSites) < $maxResults ) {
-	// add some UNRANKED results to make up the difference
-	array_splice($aRankedSites, count($aRankedSites), $maxResults, $aUnrankedSites);
-}		
-
-if ( $numUrls > $maxResults ) {
-	$remaining = $numUrls - $maxResults;
-	array_push($aRankedSites, array("label" => ( $remaining > 100 ? "100+ more URLs" : "$remaining more URLs" ) . ", refine further", "value" => "0"));
-}
-mysql_free_result($result);
-
-//echo JSON to our jQueryUI auto-complete box
-$response = json_encode($aRankedSites);
 
 $jsonp = getParam("jsonp");
 if ( $jsonp ) {
