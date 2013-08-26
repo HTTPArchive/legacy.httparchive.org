dbf75e70595b9ff54d9580ed5c9dd55a37ece6df
stevesoudersorg@gmail.com <stevesoudersorg@gmail.com@fc7d47d3-c008-acd5-f51f-d19787b8a02f>
2013-08-26 17:19:13 +0000

    temporarily save dumps in BOTH formats (a separate requests table with the naming convention "requests_crawlid").
    git-svn-id: http://httparchive.googlecode.com/svn/trunk@1253 fc7d47d3-c008-acd5-f51f-d19787b8a02f

--- bulktest/batch_process.php
+++ bulktest/batch_process.php
@@ -73,6 +73,10 @@ if ( 0 === totalNotDone() ) {
 		require_once("copy.php");
 
 		updateCrawl($labelFromRun, $gArchive, $locations[0], array( "finishedDateTime" => time() ));
+		
+		// save stuff during the transition to the new DB schema
+		exportCrawlToTable($labelFromRun);
+		dumpCrawl2($labelFromRun, "All", null, false, true); // CSV is faster
 
 		cprint(date("G:i") . ": DONE with crawl!");
 		exit(0);
