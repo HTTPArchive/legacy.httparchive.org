f1e8f8ec693a6f34643969362a4d042968404a58
Brian Kardell <bkardell@gmail.com>
2019-04-27 14:06:52 -0400

    Create has_shadow_root.js
    per https://github.com/HTTPArchive/httparchive.org/issues/141 - this is slightly complicated by the fact that I think we don't want to include native elements that have a shadow root like inputs and video in this, so... someone should probably review this to see if it makes sense.

---
+++ custom_metrics/has_shadow_root.js
@@ -0,0 +1,37 @@
+
+/* 
+  What we're looking for here is elements with non-native, 
+  author-created shadow roots.
+
+  ShadowRoots can be author-created on either 
+  
+  * the elements specifically in the allowed list 
+  
+  * on things parsed with valid custom element name productions. 
+  
+  (see https://dom.spec.whatwg.org/#dom-element-attachshadow)
+  Since we are using the parsed DOM, we can simplify the second part a little 
+  by simply checking for a dash and then whether it parsed as HTMLUnknownElement 
+  or not. 
+*/
+
+var allowed = /^(article|aside|blockquote|body|div|footer|h1|h2|h3|h4|h5|h6|header||main|nav|p|section|span)$/i
+
+function isValidCustomElementName(el) {
+    return (el.tagName.indexOf('-') !== -1 && !(el instanceof HTMLUnknownElement))
+}
+
+function hasAuthorShadowRoot(el) {
+    return (
+        (
+            isValidCustomElementName(el) 
+            || 
+            allowed.test(el.tagName)
+        ) 
+        && 
+        el.shadowRoot
+    )
+}
+
+return Array.from(document.querySelectorAll('*'))
+            .some(hasAuthorShadowRoot)
