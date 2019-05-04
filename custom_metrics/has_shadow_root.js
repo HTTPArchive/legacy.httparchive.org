b2f5911329cd31b3e177f06f0e3128819e92593b
Brian Kardell <bkardell@gmail.com>
2019-05-04 09:00:59 -0400

    Update has_shadow_root.js
    Update the way we check for valid custom element name after some discussion in order to avoid potential false positives.

--- custom_metrics/has_shadow_root.js
+++ custom_metrics/has_shadow_root.js
@@ -11,14 +11,22 @@
   
   (see https://dom.spec.whatwg.org/#dom-element-attachshadow)
   Since we are using the parsed DOM, we can simplify the second part a little 
-  by simply checking for a dash and then whether it parsed as HTMLUnknownElement 
-  or not. 
 */
 
 var allowed = /^(article|aside|blockquote|body|div|footer|h1|h2|h3|h4|h5|h6|header|main|nav|p|section|span)$/i
 
 function isValidCustomElementName(el) {
-    return (el.tagName.indexOf('-') !== -1 && !(el instanceof HTMLUnknownElement))
+    // it's got to have a dash
+    if ((el.tagName.indexOf('-') !== -1) {
+         // it has to be both an HTMLElement this prevents us from getting
+         // any dasherized elements that could exist in 'other embdedded'
+        if (el instanceof HTMLElement) {
+
+             // finally, it actually has to be defined as a custom element
+             return !(el instanceof HTMLUnknownElement)
+        }
+    }
+    return false
 }
 
 function hasAuthorShadowRoot(el) {
