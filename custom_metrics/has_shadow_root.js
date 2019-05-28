e5d324260a3c865cd6a9d93934614931c4287b73
Brian Kardell <bkardell@gmail.com>
2019-05-28 13:08:48 -0400

    Create has_shadow_root.js (#159)
    * Create has_shadow_root.js
    
    per https://github.com/HTTPArchive/httparchive.org/issues/141 - this is slightly complicated by the fact that I think we don't want to include native elements that have a shadow root like inputs and video in this, so... someone should probably review this to see if it makes sense.
    
    * Update has_shadow_root.js
    
    Fix double || in regexp and stringify response for consistency
    
    * Update has_shadow_root.js
    
    Update the way we check for valid custom element name after some discussion in order to avoid potential false positives.
    
    * Update has_shadow_root.js
    
    Fixed broken paren.  Tested on Web Page Test via custom metrics - returns explicit true or false. Assuming links hold up, these should illustrate successful runs..
    
    https://www.webpagetest.org/custom_metrics.php?test=190514_AV_0cdf6cffade2c8b54dc205d6b3ac596a&run=1&cached=0
    https://www.webpagetest.org/custom_metrics.php?test=190514_X3_773a477c083e5705cd680858be91f31e&run=1&cached=0

---
+++ custom_metrics/has_shadow_root.js
@@ -0,0 +1,45 @@
+
+/* 
+  What we're looking for here is elements with non-native, 
+  author-created shadow roots.
+
+  ShadowRoots can be author-created on either 
+  
+  * the elements specifically in the allowed list 
+  
+  * on things parsed with valid custom element name productions. 
+  
+  (see https://dom.spec.whatwg.org/#dom-element-attachshadow)
+  Since we are using the parsed DOM, we can simplify the second part a little 
+*/
+
+var allowed = /^(article|aside|blockquote|body|div|footer|h1|h2|h3|h4|h5|h6|header|main|nav|p|section|span)$/i
+
+function isValidCustomElementName(el) {
+    // it's got to have a dash
+    if (el.tagName.indexOf('-') !== -1) {
+         // it has to be both an HTMLElement this prevents us from getting
+         // any dasherized elements that could exist in 'other embdedded'
+        if (el instanceof HTMLElement) {
+
+             // finally, it actually has to be defined as a custom element
+             return !(el instanceof HTMLUnknownElement)
+        }
+    }
+    return false
+}
+
+function hasAuthorShadowRoot(el) {
+    return (
+        (
+            isValidCustomElementName(el) 
+            || 
+            allowed.test(el.tagName)
+        ) 
+        && 
+        el.shadowRoot
+    )
+}
+
+return "" + Array.from(document.querySelectorAll('*'))
+            .some(hasAuthorShadowRoot)
