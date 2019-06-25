7a1af419c5fd871199306f49eeb6747b8756d956
Yvo Schaap <ymschaap@gmail.com>
2019-06-25 20:25:54 +0200

    Removed instruction 2

--- custom_metrics/almanac.js
+++ custom_metrics/almanac.js
@@ -7,8 +7,7 @@
 //
 // 1. The key should be a unique identifier for the metric, if possible, like it's metric ID (eg '01.12').
 // 1a. If the value is used by multiple metrics, the key should be named according to what it's measuring (eg 'link-nodes').
-// 2. The value must evaluate to a simple type like int or string. For complex types (array, obj) use `JSON.stringify`.
-// 2a. If the value requires more than one line of code, evaluate it in an IIFE, eg `(() => { ... })()`. See `link-nodes`.
+// 2. If the value requires more than one line of code, evaluate it in an IIFE, eg `(() => { ... })()`. See `link-nodes`.
 // 3. Test your change by following the instructions at https://github.com/HTTPArchive/almanac.httparchive.org/issues/33#issuecomment-502288773.
 // 4. Submit a PR to update this file.
 
