2d87163797ad84d1418842cbece10d8cb870827c
Tom Van Goethem <tomvangoethem@gmail.com>
2020-07-28 17:12:27 +0200

    Add custom metrics for Security chapter (#174)
    * Add custom metrics for Security chapter: allow & sandbox attributes on iframe and integrity attribute on links and scripts
    
    * update security metrics

---
+++ custom_metrics/security.js
@@ -0,0 +1,20 @@
+return JSON.stringify({
+  "iframe-allow-sandbox": (() => {
+    const link = document.createElement('a');
+    return Array.from(document.querySelectorAll('iframe[allow], iframe[sandbox]')).map((iframe) => {
+      link.href = iframe.src;
+      return {
+        "allow": iframe.getAttribute('allow'),
+        "sandbox": iframe.getAttribute('sandbox'),
+        "hostname": link.origin
+      }
+    })
+  })(),
+  "sri-integrity": Array.from(document.querySelectorAll('[integrity]')).map((element) => {
+    return {
+      "integrity": element.getAttribute('integrity'),
+      "src": element.getAttribute('src'),
+      "tagname": element.tagName.toLowerCase()
+    }
+  })
+});
