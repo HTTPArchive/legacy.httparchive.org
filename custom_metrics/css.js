88f251eb01c54cc47594bff361d42d1c90a7bb2e
dooman87@gmail.com <Dmitry Pokidov>
2020-07-29 19:01:57 +1000

    Return one custom metric with array of used CSS-in-JS libraries.

--- custom_metrics/css.js
+++ custom_metrics/css.js
@@ -1,24 +1,30 @@
 //[css]
 // Uncomment the previous line for testing on webpagetest.org
 
-return JSON.stringify({
-  'has_cssinjs_styled_components': !!(document.querySelector('style[data-styled]') || document.querySelector('style[data-styled-components]')),
-  'has_cssinjs_radium': !!document.querySelector('[data-radium]'),
-  'has_cssinjs_jss': !!document.querySelector('[data-jss]'),
-  'has_cssinjs_emotion': !!document.querySelector('[data-emotion]'),
-  'has_cssinjs_goober': !!document.getElementById('_goober'),
-  'has_cssinjs_merge-styles': !!document.querySelector('[data-merge-styles]'),
-  'has_cssinjs_jsx': (() => {
-    let hasJSX = false;
-    document.querySelectorAll('style').forEach((s) => {
-      hasJSX = hasJSX || s.id.indexOf('__jsx-') === 0
-    });
+const CssInJsMap = {
+  'styled-components': document => !!document.querySelector('style[data-styled],style[data-styled-components]'),
+  'radium': document => !!document.querySelector('[data-radium]'),
+  'jss': document => !!document.querySelector('[data-jss]'),
+  'emotion': document => !!document.querySelector('[data-emotion]'),
+  'goober': document => !!document.getElementById('_goober'),
+  'merge-styles': document => !!document.querySelector('[data-merge-styles]'),
+  'jsx': document => !!document.querySelector('style[id*="__jsx-"]'),
+  'aphrodite': document => !!document.querySelector('[data-aphrodite]'),
+  'fela': document => !!document.querySelector('[data-fela-stylesheet]'),
+  'styletron': document => !!document.querySelector('[data-styletron],._styletron_hydrate_'),
+  'react-native-web': document => !!document.querySelector('#react-native-stylesheet'),
+  'glamor': document => !!document.querySelector('[data-glamor]')
+}
 
-    return hasJSX;
-  })(),
-  'has_cssinjs_aphrodite': !!document.querySelector('[data-aphrodite]'),
-  'has_cssinjs_fela': !!document.querySelector('[data-fela-stylesheet]'),
-  'has_cssinjs_styletron': !!document.querySelector('[data-styletron]') || !!document.querySelector('._styletron_hydrate_'),
-  'has_cssinjs_react-native-web': !!document.querySelector('#react-native-stylesheet'),
-  'has_cssinjs_glamor': !!document.querySelector('[data-glamor]')
+return JSON.stringify({
+  'css_in_js': (() => {
+    const usedLibraries = []
+    for (l in CssInJsMap) {
+      if (CssInJsMap[l](document)) {
+        usedLibraries.push(l);
+      }
+    }
+
+    return usedLibraries;
+  })()
 });
