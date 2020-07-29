784f5141a7ab31ff07a41b29947b2e8cb587abc0
dooman87@gmail.com <Dmitry Pokidov>
2020-07-30 05:30:38 +1000

    Inlined checks

--- custom_metrics/css.js
+++ custom_metrics/css.js
@@ -1,26 +1,26 @@
 //[css]
 // Uncomment the previous line for testing on webpagetest.org
 
-const CssInJsMap = {
-  'styled-components': document => !!document.querySelector('style[data-styled],style[data-styled-components]'),
-  'radium': document => !!document.querySelector('[data-radium]'),
-  'jss': document => !!document.querySelector('[data-jss]'),
-  'emotion': document => !!document.querySelector('[data-emotion]'),
-  'goober': document => !!document.getElementById('_goober'),
-  'merge-styles': document => !!document.querySelector('[data-merge-styles]'),
-  'jsx': document => !!document.querySelector('style[id*="__jsx-"]'),
-  'aphrodite': document => !!document.querySelector('[data-aphrodite]'),
-  'fela': document => !!document.querySelector('[data-fela-stylesheet]'),
-  'styletron': document => !!document.querySelector('[data-styletron],._styletron_hydrate_'),
-  'react-native-web': document => !!document.querySelector('#react-native-stylesheet'),
-  'glamor': document => !!document.querySelector('[data-glamor]')
-}
-
 return JSON.stringify({
   'css_in_js': (() => {
+    const CssInJsMap = {
+      'styled-components': !!document.querySelector('style[data-styled],style[data-styled-components]'),
+      'radium': !!document.querySelector('[data-radium]'),
+      'jss': !!document.querySelector('[data-jss]'),
+      'emotion': !!document.querySelector('[data-emotion]'),
+      'goober': !!document.getElementById('_goober'),
+      'merge-styles': !!document.querySelector('[data-merge-styles]'),
+      'jsx': !!document.querySelector('style[id*="__jsx-"]'),
+      'aphrodite': !!document.querySelector('[data-aphrodite]'),
+      'fela': !!document.querySelector('[data-fela-stylesheet]'),
+      'styletron': !!document.querySelector('[data-styletron],._styletron_hydrate_'),
+      'react-native-web': !!document.querySelector('#react-native-stylesheet'),
+      'glamor': !!document.querySelector('[data-glamor]')
+    }
+
     const usedLibraries = []
     for (l in CssInJsMap) {
-      if (CssInJsMap[l](document)) {
+      if (CssInJsMap[l]) {
         usedLibraries.push(l);
       }
     }
