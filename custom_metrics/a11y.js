3e6154a74a3dc235776bad4d070d914489cdc414
David Fox <david@davidjfox.com>
2020-07-29 23:30:06 -0500

    Remove heading comments
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- custom_metrics/a11y.js
+++ custom_metrics/a11y.js
@@ -1,16 +1,6 @@
 //[a11y]
 // Uncomment the previous line for testing on webpagetest.org
 
-// README! Instructions for adding a new custom metric for the Web Almanac.
-//
-// Add a new key/value pair to the return object below.
-//
-// 1. The key should be a unique identifier for the metric, if possible, like it's metric ID (eg '01.12').
-// 1a. If the value is used by multiple metrics, the key should be named according to what it's measuring (eg 'link-nodes').
-// 2. If the value requires more than one line of code, evaluate it in an IIFE, eg `(() => { ... })()`. See `link-nodes`.
-// 3. Test your change by following the instructions at https://github.com/HTTPArchive/almanac.httparchive.org/issues/33#issuecomment-502288773.
-// 4. Submit a PR to update this file.
-
 function incrementCollectorKey(collector, key) {
   if (!collector[key]) {
     collector[key] = 1;
