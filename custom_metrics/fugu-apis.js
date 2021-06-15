8730769893935ff384c68576dee1c8c2f54a55d9
Thomas Steiner <steiner.thomas@gmail.com>
2021-06-15 17:07:52 +0200

    Improve RegExen

--- custom_metrics/fugu-apis.js
+++ custom_metrics/fugu-apis.js
@@ -68,7 +68,7 @@ const patterns = {
     where: "JavaScript",
   },
   "Pointer Lock (unadjustedMovement)": {
-    regEx: /unadjustedMovement\s*\:/g,
+    regEx: /unadjustedMovement\s*\:s*/g,
     where: "JavaScript",
   },
   "WebHID": {
@@ -104,7 +104,8 @@ const patterns = {
     where: "JavaScript",
   },
   "Handwriting Recognition": {
-    regEx: /navigator\.queryHandwritingRecognizerSupport\s*\(/g,
+    regEx:
+      /navigator\.queryHandwritingRecognizerSupport\s*\(/g,
     where: "JavaScript",
   },
   "Compute Pressure": {
@@ -120,11 +121,11 @@ const patterns = {
     where: "JavaScript",
   },
   "Notification Triggers": {
-    regEx: /showTrigger\s*\:/g,
+    regEx: /showTrigger\s*\:s*/g,
     where: "JavaScript",
   },
   "Local Font Access": {
-    regEx: /navigator\.fonts\.query\s*\(\);/g,
+    regEx: /navigator\.fonts\.query\s*\(\)/g,
     where: "JavaScript",
   },
   "Multi-Screen Window Placement": {
@@ -140,19 +141,19 @@ const patterns = {
     where: "JavaScript",
   },
   "Web Share Target": {
-    regEx: /"share_target"\s*\:/g,
+    regEx: /"share_target"/g,
     where: "Web App Manifest",
   },
   "Web Share Target (Files)": {
-    regEx: /"enctype"\s*\:\s*"multipart\/form\-data"/g,
+    regEx: /"enctype"\s*\:s*"multipart\/form\-data"/g,
     where: "Web App Manifest",
   },
   "Shortcuts": {
-    regEx: /"shortcuts"\s*\:/g,
+    regEx: /"shortcuts"/g,
     where: "Web App Manifest",
   },
   "Declarative Link Capturing": {
-    regEx: /"capture_links"\s*\:/g,
+    regEx: /"capture_links"/g,
     where: "Web App Manifest",
   },
   "Tabbed Application Mode": {
@@ -180,20 +181,20 @@ const checkURLConditions = (where, url, mimeType) => {
   // includes either `.js` or `.mjs` and uses a correct-ish MIME type
   // (https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types#textjavascript).
   if (
-      (where === "JavaScript") &&
-      (/\.m?js/.test(url)) &&
-      (mimeType.toLowerCase().endsWith('script'))
+    where === "JavaScript" &&
+    /\.m?js/.test(url) &&
+    mimeType.toLowerCase().endsWith("script")
   ) {
     return true;
   }
   // If the pattern has to occur in the Web App Manifest, make sure the file
   // name includes either `.json` or `.webmanifest` and uses a MIME type that
   // ends in "json"
-  // (https://w3c.github.io/manifest/#:~:text=file%20extension%3A%20.webmanifest%20or%20.json%3F).  
+  // (https://w3c.github.io/manifest/#:~:text=file%20extension%3A%20.webmanifest%20or%20.json%3F).
   if (
-    (where === "Web App Manifest") &&
-    (/\.webmanifest|\.json/.test(url)) &&
-    (mimeType.toLowerCase().endsWith('json'))
+    where === "Web App Manifest" &&
+    /\.webmanifest|\.json/.test(url) &&
+    mimeType.toLowerCase().endsWith("json")
   ) {
     return true;
   }
@@ -209,13 +210,19 @@ responseBodies.forEach((har) => {
     if (value.regEx.test(har.response_body)) {
       // Ignore the optional encoding, e.g.,
       // `application/manifest+json; charset=utf-8`.
-      const mimeType = har.response_headers['content-type'].split(';')[0].trim();
+      const mimeType = har.response_headers["content-type"]
+        .split(";")[0]
+        .trim();
       if (result[key] && !result[key].includes(har.url)) {
-        if (checkURLConditions(value.where, har.url, mimeType)) {
+        if (
+          checkURLConditions(value.where, har.url, mimeType)
+        ) {
           result[key].push(har.url);
         }
       } else {
-        if (checkURLConditions(value.where, har.url, mimeType)) {
+        if (
+          checkURLConditions(value.where, har.url, mimeType)
+        ) {
           result[key] = [har.url];
         }
       }
