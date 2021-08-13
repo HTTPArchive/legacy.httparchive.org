08ed4cd698e018a2604f2bd04a3dd1e6814c620c
Thomas Steiner <steiner.thomas@gmail.com>
2021-08-13 14:40:52 +0200

    Add `prefers-color-scheme` custom metric (#227)
    * Add prefers-color-scheme custom metric
    
    * Add link testing
    
    * Use $WPT_BODIES, too
    
    * Update custom_metrics/prefers-color-scheme.js
    
    * Apply suggestions from code review
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Move to css.js and simplify to only use bodies and link
    
    * Apply suggestions from code review
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Prettier
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- custom_metrics/css.js
+++ custom_metrics/css.js
@@ -1,24 +1,35 @@
 //[css]
 // Uncomment the previous line for testing on webpagetest.org
 
+const PREFERS_COLOR_SCHEME_REGEXP =
+  /@media\s+\(\s*prefers-color-scheme\s*:\s*(?:dark|light)\s*\)\s*\{[^\}]*\}/gms;
+
+const bodies = $WPT_BODIES;
+
 return JSON.stringify({
-  'css_in_js': (() => {
+  css_in_js: (() => {
     const CssInJsMap = {
-      'Styled Components': !!document.querySelector('style[data-styled],style[data-styled-components]'),
-      'Radium': !!document.querySelector('[data-radium]'),
-      'JSS': !!document.querySelector('[data-jss]'),
-      'Emotion': !!document.querySelector('[data-emotion]'),
-      'Goober': !!document.getElementById('_goober'),
+      'Styled Components': !!document.querySelector(
+        'style[data-styled],style[data-styled-components]',
+      ),
+      Radium: !!document.querySelector('[data-radium]'),
+      JSS: !!document.querySelector('[data-jss]'),
+      Emotion: !!document.querySelector('[data-emotion]'),
+      Goober: !!document.getElementById('_goober'),
       'Merge Styles': !!document.querySelector('[data-merge-styles]'),
       'Styled Jsx': !!document.querySelector('style[id*="__jsx-"]'),
-      'Aphrodite': !!document.querySelector('[data-aphrodite]'),
-      'Fela': !!document.querySelector('[data-fela-stylesheet]'),
-      'Styletron': !!document.querySelector('[data-styletron],._styletron_hydrate_'),
-      'React Native for Web': !!document.querySelector('#react-native-stylesheet'),
-      'Glamor': !!document.querySelector('[data-glamor]')
-    }
+      Aphrodite: !!document.querySelector('[data-aphrodite]'),
+      Fela: !!document.querySelector('[data-fela-stylesheet]'),
+      Styletron: !!document.querySelector(
+        '[data-styletron],._styletron_hydrate_',
+      ),
+      'React Native for Web': !!document.querySelector(
+        '#react-native-stylesheet',
+      ),
+      Glamor: !!document.querySelector('[data-glamor]'),
+    };
 
-    const usedLibraries = []
+    const usedLibraries = [];
     for (l in CssInJsMap) {
       if (CssInJsMap[l]) {
         usedLibraries.push(l);
@@ -26,5 +37,24 @@ return JSON.stringify({
     }
 
     return usedLibraries;
-  })()
+  })(),
+  // Checks in two passes:
+  // 1. The response body of same- and cross-origin stylesheets.
+  // 2. The `link[media]` attribute of conditionally loaded stylesheets in the
+  //    ternary expression if step 1. returns `false`.
+  prefersColorScheme:
+    // For all stylesheets…
+    // …check if the `prefers-color-scheme` RegExp matches…
+    // If even just one of the stylesheets matches, return `true`.
+    bodies.some((request) => {
+      return (
+        request.type === 'Stylesheet' &&
+        PREFERS_COLOR_SCHEME_REGEXP.test(request.response_body || '')
+      );
+    }) ||
+    // If none of the stylesheets match, alternatively check if any of the
+    // stylesheet `link`s load conditionally based on `prefers-color-scheme`.
+    document.querySelectorAll(
+      'link[rel="stylesheet"][media*="prefers-color-scheme"]',
+    ).length > 0,
 });
