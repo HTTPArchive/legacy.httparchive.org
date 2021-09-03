db2af3617ae136079573c69d83fa6c254c42dcb6
demianrenzulli <demian77@gmail.com>
2021-09-03 16:46:01 -0400

    Updating dark mode metric.

--- custom_metrics/css.js
+++ custom_metrics/css.js
@@ -2,7 +2,7 @@
 // Uncomment the previous line for testing on webpagetest.org
 
 const PREFERS_COLOR_SCHEME_REGEXP =
-  /@media\s+\(\s*prefers-color-scheme\s*:\s*(?:dark|light)\s*\)\s*\{[^\}]*\}/gms;
+  /(@media\s*\(\s*prefers-color-scheme\s*:\s*(?:dark|light)\s*\)\s*\{[^\}]*\}|window.matchMedia\([\'"]\s*\(prefers-color-scheme\s*:\s*(?:dark|light)\)[\'"])/gms;
 
 const bodies = $WPT_BODIES;
 
@@ -39,20 +39,15 @@ return JSON.stringify({
     return usedLibraries;
   })(),
   // Checks in two passes:
-  // 1. The response body of same- and cross-origin stylesheets.
+  // 1. The response bodies.
   // 2. The `link[media]` attribute of conditionally loaded stylesheets in the
   //    ternary expression if step 1. returns `false`.
   prefersColorScheme:
-    // For all stylesheets…
-    // …check if the `prefers-color-scheme` RegExp matches…
-    // If even just one of the stylesheets matches, return `true`.
     bodies.some((request) => {
-      return (
-        request.type === 'Stylesheet' &&
-        PREFERS_COLOR_SCHEME_REGEXP.test(request.response_body || '')
+      return (PREFERS_COLOR_SCHEME_REGEXP.test(request.response_body || '')
       );
     }) ||
-    // If none of the stylesheets match, alternatively check if any of the
+    // If none of the response bodies match, alternatively check if any of the
     // stylesheet `link`s load conditionally based on `prefers-color-scheme`.
     document.querySelectorAll(
       'link[rel="stylesheet"][media*="prefers-color-scheme"]',
